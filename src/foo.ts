c76ae73075fd96db2318f36e15d153b0bb88940c
Pavol Hejny <me@pavolhejny.com>
2023-09-27 17:12:26 +0200

    Fix lint

--- src/foo.ts
+++ src/foo.ts
@@ -1,4 +1,3 @@
 export function foo() {
-    // eslint-disable-next-line no-console
-    console.log('testing that something is included in the bundle');
+    return 'testing that something is included in the bundle';
 }
