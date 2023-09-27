507cc57bd9baf072a8010392350b13d0c3aa6eca
Pavol Hejny <me@pavolhejny.com>
2023-09-27 17:09:28 +0200

    Change boilerplate spacetrim -> ptp

--- src/nesting/block-constants.ts
+++
@@ -1,5 +0,0 @@
-const nonce = `38ea8d83-fe54-47cd-9519-90a90c58596f_${Math.floor(
-    Math.random() * 100000,
-)}`;
-export const SPACE = `__SPACE_${nonce}__`;
-export const NEWLINE = `__NEWLINE_${nonce}__`;
