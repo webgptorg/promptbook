5d0c6735ceaffb117d5c4ad5501c02d6c539107b
Pavol Hejny <me@pavolhejny.com>
2023-09-29 12:38:43 +0200

    Add some dummy test

---
+++ src/foo.test.ts
@@ -0,0 +1,7 @@
+import { foo } from './foo';
+
+describe('the errored modules', () => {
+    it(`should crash when there is a syntax-error in the module`, () => {
+        expect(foo()).toBeTruthy();
+    });
+});
