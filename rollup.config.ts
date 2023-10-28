1b7477adb9374d1b7d617dee3911bd48edb8dbc9
Pavol Hejny <me@pavolhejny.com>
2023-10-28 03:26:26 +0200

    Try to fix browser/node package
    The package is still not working in the browser, try splitting exports to fix it

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-    input: './src/index.ts',
+export default ['node', 'browser', 'foo' /* <- !!! Remove */].map((name) => ({
+    input: `./src/${name}.index.ts`,
     output: [
         {
-            file: './dist/umd/index.js',
-            name: 'ptp',
+            file: `./dist/umd/${name}.umd.js`,
+            name: `ptp-${name}`,
             format: 'umd',
             sourcemap: true,
         },
         {
-            file: './dist/esm/index.js',
+            file: `./dist/esm/${name}.es.js`,
             format: 'es',
             sourcemap: true,
         },
     ],
     plugins: [typescript({ tsconfig: './tsconfig.json' })],
-};
+}));
