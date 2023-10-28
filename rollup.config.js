3645f36ccaabd3d00d29db147d0c0a09eeaa54c7
Pavol Hejny <me@pavolhejny.com>
2023-10-28 16:32:58 +0200

    Dynamically detect which packages will be emited

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
+import { readdirSync } from 'fs';
+import { join } from 'path';
 
-export default ['node', 'browser' /* <- !!! Automatic from _packages */].map((name) => ({
+const packages = readdirSync(join(__dirname, 'src/_packages'), { recursive: false, withFileTypes: true })
+    .filter((dirent) => dirent.isFile())
+    .filter((dirent) => dirent.name.endsWith('.index.ts'))
+    .map((dirent) => dirent.name.split('.').shift());
+
+export default packages.map((name) => ({
     input: `./src/_packages/${name}.index.ts`,
     output: [
         {
