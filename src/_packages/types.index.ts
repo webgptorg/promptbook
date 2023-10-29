b9ef6fb6fdc488404ed8897054b1d71cac30e055
Pavol Hejny <me@pavolhejny.com>
2023-10-29 13:55:23 +0100

    Export types as library

--- src/_packages/types.index.ts
+++ src/_packages/types.index.ts
@@ -133,7 +133,6 @@ export {
     PromptTemplateJson,
     PromptTemplateParameterJson,
     PromptTemplatePipelineJson,
-    PromptTemplatePipelineLibrary,
     PromptTemplatePipelineString,
     PtpExecutor,
     ScriptExecutionTools,
