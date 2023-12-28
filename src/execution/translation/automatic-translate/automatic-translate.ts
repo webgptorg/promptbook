6f31832e37982daa0f5b2840ea1b1a60176cb608
Pavol Hejny <me@pavolhejny.com>
2023-12-28 13:26:44 +0100

    Remove translateArticles

--- src/execution/translation/automatic-translate/automatic-translate.ts
+++ src/execution/translation/automatic-translate/automatic-translate.ts
@@ -3,7 +3,6 @@
 import chalk from 'chalk';
 import commander from 'commander';
 import { createBestTranslator } from './automatic-translators/createBestTranslator';
-import { translateArticles } from './translateArticles';
 import { translateMessages } from './translateMessages';
 
 // TODO: This should be some prototype of the generator
@@ -18,17 +17,13 @@ async function main() {
     program.option('--from <language>', `From what language to translate`);
     program.option('--to <language>', `To what language to translate`);
     program.option('--messages', `Translate messages`);
-    program.option('--articles', `Translate articles`);
 
     program.parse(process.argv);
-    const { from, to, messages, articles } = program.opts();
+    const { from, to, messages } = program.opts();
 
     const automaticTranslator = createBestTranslator({ from, to });
 
     if (messages) {
         await translateMessages({ automaticTranslator, from, to });
-    }
-    if (articles) {
-        await translateArticles({ automaticTranslator, from, to });
     }
 }
