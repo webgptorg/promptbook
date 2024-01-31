19fe9a7396d49ded807f35a1e648c37d6de6d18f
Pavol Hejny <me@pavolhejny.com>
2024-01-31 20:37:44 +0100

    Revert "Add support for counting emoji characters (not working emojis)"
    This reverts commit 18f1f86e92b32aaf77f0ef5f1a6f8265ed7b4fe7.

--- src/utils/expectation-counters/countCharacters.ts
+++ src/utils/expectation-counters/countCharacters.ts
@@ -4,8 +4,5 @@ import type { ExpectationAmount } from '../../types/PromptTemplatePipelineJson/P
  * Counts number of characters in the text
  */
 export function countCharacters(text: string): ExpectationAmount {
-    text = text.replace(/\p{Emoji}[\u{FE00}-\u{FE0F}]/gu, 'x');
-    text = text.replace(/\p{Emoji}\u{200D}\p{Emoji}/gu, 'x');
-    text = text.replace(/\p{Emoji}/gu, 'x');
     return text.length;
 }
