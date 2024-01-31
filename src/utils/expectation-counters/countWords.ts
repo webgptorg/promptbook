e33ca3bf171f7f4a6ed4aa7c735737a70020b571
Pavol Hejny <me@pavolhejny.com>
2024-01-31 19:29:56 +0100

    Add additional test cases for countWords function

--- src/utils/expectation-counters/countWords.ts
+++ src/utils/expectation-counters/countWords.ts
@@ -1,12 +1,12 @@
+import { removeDiacritics } from 'n12';
 import type { ExpectationAmount } from '../../types/PromptTemplatePipelineJson/PromptTemplateJson';
 
 /**
  * Counts number of words in the text
  */
 export function countWords(text: string): ExpectationAmount {
-    if (text === '') {
-        return 0;
-    }
+    text = text.replace(/[\p{Emoji}]/gu, 'a');
+    text = removeDiacritics(text);
 
-    return text.trim().split(/\s+/).length;
+    return text.split(/[^a-zа-я0-9]+/i).filter((word) => word.length > 0).length;
 }
