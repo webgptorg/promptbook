92c4d096ef99775f5d9a177ef0fdc8c22990f7b1
Pavol Hejny <me@pavolhejny.com>
2024-02-01 15:10:51 +0100

    Better executionReportJsonToString

--- src/types/execution-report/executionReportJsonToString.ts
+++ src/types/execution-report/executionReportJsonToString.ts
@@ -29,32 +29,50 @@ export function executionReportJsonToString(executionReportJson: ExecutionReport
                 (block) => `
                 ## ${promptExecution.prompt.title}
 
+                -   MODEL VARIANT ${promptExecution.prompt.modelRequirements.modelVariant}
+                -   MODEL NAME \`${promptExecution.result?.model}\` (requested \`${
+                    promptExecution.prompt.modelRequirements.modelName
+                }\`)
+
                 ### Prompt
 
                 \`\`\`
                 ${block(promptExecution.prompt.content)}
                 \`\`\`
 
-                ### Result
-
-                \`\`\`
-                ${block(promptExecution.result?.content || '')}
-                \`\`\`
-
-                ### Model requirements
-
-                \`\`\`
-                ${block(JSON.stringify(promptExecution.prompt.modelRequirements, null, 4))}
-                \`\`\`
-
-                ### Error
-
-                \`\`\`
-                ${block(promptExecution.error?.message || '')}
-                \`\`\`
-
-            `,
+                `,
             );
+
+        if (promptExecution.result) {
+            executionReportString +=
+                '\n\n\n\n' +
+                spaceTrim(
+                    (block) => `
+
+                        ### Result
+
+                        \`\`\`
+                        ${block(promptExecution.result!.content || '')}
+                        \`\`\`
+                    `,
+                );
+        }
+
+        if (promptExecution.error) {
+            executionReportString +=
+                '\n\n\n\n' +
+                spaceTrim(
+                    (block) => `
+
+                        ### Error
+
+                        \`\`\`
+                        ${block(promptExecution.error!.message || '')}
+                        \`\`\`
+
+                    `,
+                );
+        }
     }
 
     executionReportString = prettifyMarkdown(executionReportString);
