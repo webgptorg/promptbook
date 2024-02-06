d0f709d20f71ebdb1b730822c91b16b917f791bf
Pavol Hejny <me@pavolhejny.com>
2024-02-06 14:45:41 +0100

    Custom postprocessing functions (with dependencies)

---
+++ src/execution/plugins/script-execution-tools/custom-function-with-dependencies.test.ts
@@ -0,0 +1,102 @@
+import { describe, expect, it } from '@jest/globals';
+import spaceTrim from 'spacetrim';
+import { countCharacters, countWords } from '../../../_packages/utils.index';
+import { PromptTemplatePipeline } from '../../../classes/PromptTemplatePipeline';
+import { promptTemplatePipelineStringToJson } from '../../../conversion/promptTemplatePipelineStringToJson';
+import { PromptTemplatePipelineString } from '../../../types/PromptTemplatePipelineString';
+import { createPtpExecutor } from '../../createPtpExecutor';
+import { MockedEchoNaturalExecutionTools } from '../natural-execution-tools/mocked/MockedEchoNaturalExecutionTools';
+import { CallbackInterfaceTools } from '../user-interface-execution-tools/callback/CallbackInterfaceTools';
+import { JavascriptEvalExecutionTools } from './javascript/JavascriptEvalExecutionTools';
+
+describe('createPtpExecutor + custom function with dependencies', () => {
+    const ptbJson = promptTemplatePipelineStringToJson(
+        spaceTrim(`
+            # Custom functions
+
+            Show how to use custom postprocessing functions with dependencies
+
+            -   PTBK VERSION 1.0.0
+            -   INPUT  PARAMETER {yourName} Name of the hero
+
+            ## Question
+
+            -   SIMPLE TEMPLATE
+            -   POSTPROCESSING addHello
+            -   POSTPROCESSING withStatistics
+
+            \`\`\`markdown
+            {yourName} the Evangelist
+            \`\`\`
+
+            -> {greeting}
+         `) as PromptTemplatePipelineString,
+    );
+
+    const ptp = PromptTemplatePipeline.fromJson(ptbJson);
+    const ptpExecutor = createPtpExecutor({
+        ptp,
+        tools: {
+            natural: new MockedEchoNaturalExecutionTools({ isVerbose: true }),
+            script: [
+                new JavascriptEvalExecutionTools({
+                    isVerbose: true,
+
+                    // Note: [🕎]
+                    functions: {
+                        addHello(value) {
+                            return `Hello ${value}`;
+                        },
+                        withStatistics(value) {
+                            // Note: Testing custom function with dependencies
+                            return value + ` (${countCharacters(value)} characters, ${countWords(value)} words)`;
+                        },
+                    },
+                }),
+            ],
+            userInterface: new CallbackInterfaceTools({
+                isVerbose: true,
+                async callback() {
+                    return 'Hello';
+                },
+            }),
+        },
+        settings: {
+            maxExecutionAttempts: 3,
+        },
+    });
+
+    it('should use custom postprocessing function', () => {
+        expect(ptpExecutor({ yourName: 'Matthew' }, () => {})).resolves.toMatchObject({
+            isSuccessful: true,
+            outputParameters: {
+                greeting: 'Hello Matthew the Evangelist (28 characters, 4 words)',
+            },
+        });
+
+        expect(ptpExecutor({ yourName: 'Mark' }, () => {})).resolves.toMatchObject({
+            isSuccessful: true,
+            outputParameters: {
+                greeting: 'Hello Mark the Evangelist (25 characters, 4 words)',
+            },
+        });
+
+        expect(ptpExecutor({ yourName: 'Luke' }, () => {})).resolves.toMatchObject({
+            isSuccessful: true,
+            outputParameters: {
+                greeting: 'Hello Luke the Evangelist (25 characters, 4 words)',
+            },
+        });
+
+        expect(ptpExecutor({ yourName: 'John' }, () => {})).resolves.toMatchObject({
+            isSuccessful: true,
+            outputParameters: {
+                greeting: 'Hello John the Evangelist (25 characters, 4 words)',
+            },
+        });
+    });
+});
+
+/**
+ * TODO: !!!! custom-function-async.test.ts
+ */
