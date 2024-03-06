c264f670098136127e114979952342c38cb016b1
Pavol Hejny <me@pavolhejny.com>
2024-03-06 14:27:03 +0100

    Fix tests

--- src/conversion/promptTemplatePipelineStringToJson.test.ts
+++ src/conversion/promptTemplatePipelineStringToJson.test.ts
@@ -6,6 +6,9 @@ import { promptTemplatePipelineStringToJson } from './promptTemplatePipelineStri
 
 describe('promptTemplatePipelineStringToJson', () => {
     const samplesDir = '../../samples/templates';
+
+    console.log('join(__dirname, samplesDir)', join(__dirname, samplesDir));
+
     const samples = readdirSync(join(__dirname, samplesDir), { withFileTypes: true, recursive: false })
         //                         <- Note: In production it is not good practice to use synchronous functions
         //                                  But this is only a test before the build, so it is okay
