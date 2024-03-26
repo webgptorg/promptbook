6270244eac9036c42d8ab3f8ac30aefb7862e3c2
Pavol Hejny <me@pavolhejny.com>
2024-03-26 14:26:11 +0100

    Cleanup

--- packages/utils/bin/promptbook-cli.js
+++ packages/utils/bin/promptbook-cli.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Hello from promptbook-cli.js!');
-
-const colors = require('colors');
 const {
     __: { prettifyPromptbookStringCli },
 } = require('../umd/index.umd.js');
 
 prettifyPromptbookStringCli();
-
 
 /**
  * TODO: [🕌] When more functionalities, rename
