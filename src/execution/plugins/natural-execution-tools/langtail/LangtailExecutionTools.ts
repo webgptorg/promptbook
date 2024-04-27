0c4e973fc2469c2d780fb1524d39e1580e319b55
Pavol Hejny <me@pavolhejny.com>
2024-04-27 16:46:05 +0200

    LangtailExecutionTools

--- src/execution/plugins/natural-execution-tools/langtail/LangtailExecutionTools.ts
+++ src/execution/plugins/natural-execution-tools/langtail/LangtailExecutionTools.ts
@@ -1,1 +1,11 @@
-LangtailExecutionTools;
+import type { NaturalExecutionTools } from '../../../NaturalExecutionTools';
+import { OpenAiExecutionTools } from '../openai/OpenAiExecutionTools';
+
+/**
+ * Execution Tools for calling OpenAI API.
+ */
+export class LangtailExecutionTools extends OpenAiExecutionTools implements NaturalExecutionTools {}
+
+/**
+ * TODO: !!! Make lib which exports this
+ */
