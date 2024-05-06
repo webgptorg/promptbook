e3828559a339c63deee34156072c7f367a6cb48d
Pavol Hejny <me@pavolhejny.com>
2024-05-06 19:50:46 +0200

    TODOs

--- src/types/PromptbookJson/PromptTemplateJson.ts
+++ src/types/PromptbookJson/PromptTemplateJson.ts
@@ -28,7 +28,7 @@ export type LlmTemplateJson = PromptTemplateJsonCommon & {
      * Requirements for the model
      * - This is required only for executionType PROMPT_TEMPLATE
      */
-    readonly modelRequirements: ModelRequirements; // <- TODO: !!! Make Partial<ModelRequirements>;
+    readonly modelRequirements: ModelRequirements; // <- TODO: !!! Make Partial<ModelRequirements> and optional;
 };
 
 /**
