1b622ae1999838695e0860f8a3665503df45f02d
Pavol Hejny <me@pavolhejny.com>
2024-06-03 15:19:49 +0200

    🆚 Update version in config
    🔼 This commit was automatically generated by map scripts

--- src/version.ts
+++ src/version.ts
@@ -3,4 +3,4 @@ import type { string_version } from './types/typeAliases';
 /**
  * The version of the Promptbook library
  */
-export const PROMPTBOOK_VERSION: string_version = '0.52.0-24';
+export const PROMPTBOOK_VERSION: string_version = '0.52.0-25';
