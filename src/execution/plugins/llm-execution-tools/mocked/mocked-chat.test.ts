099067e61df3b0f8509de60b9534a13fc81c7e00
Pavol Hejny <me@pavolhejny.com>
2024-06-15 19:48:15 +0200

    Fix tests

--- src/execution/plugins/llm-execution-tools/mocked/mocked-chat.test.ts
+++ src/execution/plugins/llm-execution-tools/mocked/mocked-chat.test.ts
@@ -80,6 +80,57 @@ describe('createPromptbookExecutor + MockedEchoExecutionTools with sample chat p
                 promptbookUsedVersion: PROMPTBOOK_VERSION,
             },
             outputParameters: {},
+            usage: {
+                input: {
+                    charactersCount: {
+                        value: 0,
+                    },
+                    linesCount: {
+                        value: 0,
+                    },
+                    pagesCount: {
+                        value: 0,
+                    },
+                    paragraphsCount: {
+                        value: 0,
+                    },
+                    sentencesCount: {
+                        value: 0,
+                    },
+                    tokensCount: {
+                        value: 0,
+                    },
+                    wordsCount: {
+                        value: 0,
+                    },
+                },
+                output: {
+                    charactersCount: {
+                        value: 0,
+                    },
+                    linesCount: {
+                        value: 0,
+                    },
+                    pagesCount: {
+                        value: 0,
+                    },
+                    paragraphsCount: {
+                        value: 0,
+                    },
+                    sentencesCount: {
+                        value: 0,
+                    },
+                    tokensCount: {
+                        value: 0,
+                    },
+                    wordsCount: {
+                        value: 0,
+                    },
+                },
+                price: {
+                    value: 0,
+                },
+            },
         }));
 
     /*
