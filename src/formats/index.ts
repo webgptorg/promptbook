aab6dc397735d046074edf515d3ccf55492b65c0
Pavol Hejny <me@pavolhejny.com>
2024-06-26 19:04:30 +0200

    ListFormatDefinition

--- src/formats/index.ts
+++ src/formats/index.ts
@@ -1,7 +1,8 @@
 import { JsonFormatDefinition } from './json/JsonFormatDefinition';
+import { ListFormatDefinition } from './list/ListFormatDefinition';
 import { XmlFormatDefinition } from './xml/XmlFormatDefinition';
 
 /**
  * Definitions for all formats supported by Promptbook
  */
-export const FORMAT_DEFINITIONS = [JsonFormatDefinition, XmlFormatDefinition];
+export const FORMAT_DEFINITIONS = [JsonFormatDefinition, XmlFormatDefinition, ListFormatDefinition];
---
+++ src/formats/list/ListFormatDefinition.ts
@@ -0,0 +1,31 @@
+import { just } from '../../utils/just';
+import type { FormatDefinition } from '../_common/FormatDefinition';
+
+/**
+ * Definition for list of multiple items
+ *
+ * Note: list is just a string with multiple lines or multiple items separated by comma
+ */
+export const ListFormatDefinition: FormatDefinition<string /* <-[0] */, string /* <-[👨‍⚖️] */, object /* <-[1] */> = {
+    name: 'LIST',
+
+    aliases: ['ARRAY', 'BULLETS'],
+
+    isValid(value, schema): value is string /* <-[0] */ {
+        just(value /* <- TODO: Use value here */);
+        just(schema /* <- TODO: Use schema here */);
+        return true;
+    },
+
+    canBeValid(partialValue, schema): partialValue is string /* <-[0] */ {
+        just(partialValue /* <- TODO: Use partialValue here */);
+        just(schema /* <- TODO: Use schema here */);
+        return true;
+    },
+
+    heal(value, schema) {
+        just(value /* <- TODO: Use partialValue here */);
+        just(schema /* <- TODO: Use schema here */);
+        throw new Error('Not implemented');
+    },
+};
