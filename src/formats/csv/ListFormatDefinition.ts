ff88fabc86fb0b372db54892042cb8712c2e3cd8
Pavol Hejny <me@pavolhejny.com>
2024-06-26 19:11:08 +0200

    CsvFormatDefinition

---
+++ src/formats/csv/ListFormatDefinition.ts
@@ -0,0 +1,29 @@
+import { just } from '../../utils/just';
+import type { FormatDefinition } from '../_common/FormatDefinition';
+
+/**
+ * Definition for CSV spreadsheet
+ */
+export const CsvFormatDefinition: FormatDefinition<string /* <-[0] */, string /* <-[👨‍⚖️] */, object /* <-[1] */> = {
+    name: 'CSV',
+
+    aliases: ['SPREADSHEET', 'TABLE'],
+
+    isValid(value, schema): value is string /* <-[0] */ {
+        just(value /* <- TODO: Use value here */);
+        just(schema /* <- TODO: Use schema here */);
+        return true;
+    },
+
+    canBeValid(partialValue, schema): partialValue is string /* <-[0] */ {
+        just(partialValue /* <- TODO: Use partialValue here */);
+        just(schema /* <- TODO: Use schema here */);
+        return true;
+    },
+
+    heal(value, schema) {
+        just(value /* <- TODO: Use partialValue here */);
+        just(schema /* <- TODO: Use schema here */);
+        throw new Error('Not implemented');
+    },
+};
--- src/formats/index.ts
+++ src/formats/index.ts
@@ -1,3 +1,4 @@
+import { CsvFormatDefinition } from './csv/ListFormatDefinition';
 import { JsonFormatDefinition } from './json/JsonFormatDefinition';
 import { ListFormatDefinition } from './list/ListFormatDefinition';
 import { XmlFormatDefinition } from './xml/XmlFormatDefinition';
@@ -5,4 +6,9 @@ import { XmlFormatDefinition } from './xml/XmlFormatDefinition';
 /**
  * Definitions for all formats supported by Promptbook
  */
-export const FORMAT_DEFINITIONS = [JsonFormatDefinition, XmlFormatDefinition, ListFormatDefinition];
+export const FORMAT_DEFINITIONS = [
+    JsonFormatDefinition,
+    XmlFormatDefinition,
+    ListFormatDefinition,
+    CsvFormatDefinition,
+];
