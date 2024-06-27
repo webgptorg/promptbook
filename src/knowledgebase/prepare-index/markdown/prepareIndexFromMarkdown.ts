14b0b35a7bdac106a624f1e522483d027eb55745
Pavol Hejny <me@pavolhejny.com>
2024-06-27 17:07:20 +0200

    Draft of `prepareIndexFromMarkdown`

---
+++ src/knowledgebase/prepare-index/markdown/prepareIndexFromMarkdown.ts
@@ -0,0 +1,19 @@
+import { ExecutionTools } from '../../../execution/ExecutionTools';
+import { string_href } from '../../../types/typeAliases';
+import { just } from '../../../utils/just';
+
+export async function prepareIndexFromMarkdown(options: { tools: ExecutionTools }): Promise<{
+    indexes: Array<{
+        title: string;
+        content: string;
+        href: string_href;
+    }>;
+}> {
+    const { tools } = options;
+
+    just(tools);
+
+    return {
+        indexes: [],
+    };
+}
