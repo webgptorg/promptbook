76424d1d360f874258b71dd9b13b0e605e5604b0
Pavol Hejný <me@pavolhejny.com>
2024-07-04 16:31:34 +0200

    Fix and enhance script  Make Promptbook library

--- scripts/make-promptbook-library/make-promptbook-library.ts
+++ scripts/make-promptbook-library/make-promptbook-library.ts
@@ -4,17 +4,25 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 import colors from 'colors';
+import commander from 'commander';
 import { writeFile } from 'fs/promises';
 import { join } from 'path';
 import { createLibraryFromDirectory } from '../../src/library/constructors/createLibraryFromDirectory';
 import { libraryToJson } from '../../src/library/constructors/libraryToJson';
+import { commit } from '../utils/autocommit/commit';
 
 if (process.cwd() !== join(__dirname, '../..')) {
     console.error(colors.red(`CWD must be root of the project`));
     process.exit(1);
 }
 
-makePromptbookLibrary()
+const program = new commander.Command();
+program.option('--commit', `Auto commit`, false);
+program.parse(process.argv);
+
+const { commit: isCommited } = program.opts();
+
+makePromptbookLibrary({ isCommited })
     .catch((error) => {
         console.error(colors.bgRed(error.name || 'NamelessError'));
         console.error(error);
@@ -24,30 +32,36 @@ makePromptbookLibrary()
         process.exit(0);
     });
 
-async function makePromptbookLibrary() {
+async function makePromptbookLibrary({ isCommited }: { isCommited: boolean }) {
     console.info(`📖 Make Promptbook library`);
 
     const promptbookSourceDir = 'promptbook-library';
 
     const library = await createLibraryFromDirectory(promptbookSourceDir, {
+        isVerbose: true,
         isRecursive: true,
     });
 
-    const libraryJson = libraryToJson(library);
+    const libraryJson = await libraryToJson(library);
     const libraryJsonString = JSON.stringify(libraryJson);
+
+    const libraryJsonFilePath = join(promptbookSourceDir, 'promptbook-library.json');
     const libraryJsonFileContent = libraryJsonString + '\n';
+
+    const libraryTypescriptFilePath = join(promptbookSourceDir, 'promptbook-library.ts');
     const libraryTypescriptFileContent = 'export default ' + libraryJsonString + ';\n';
 
-    // TODO: [🏳‍🌈] Finally take one of .json vs .ts
-    await writeFile(
-        `${promptbookSourceDir}/promptbook-library.json` /* <- Note: [🏳‍🌈] Maybe make .ts file (not .json) to avoid support of json files in bundle */,
-        libraryJsonFileContent,
-        'utf-8',
-    );
+    // TODO: [🏳‍🌈] Finally take one of .json vs .ts (using .ts file (not .json) to avoid support of json files in bundle )
+    await writeFile(libraryJsonFilePath, libraryJsonFileContent, 'utf-8');
+    console.info(colors.green(`Maked ${libraryJsonFilePath}`));
+    await writeFile(libraryTypescriptFilePath, libraryTypescriptFileContent, 'utf-8');
+    console.info(colors.green(`Maked ${libraryTypescriptFilePath}`));
 
-    await writeFile(`${promptbookSourceDir}/promptbook-library.ts`, libraryTypescriptFileContent, 'utf-8');
+    if (isCommited) {
+        await commit(promptbookSourceDir, `📖 Make Promptbook library`);
+    }
 
-    console.info(`[ Done 📖 Make  Promptbook library ]`);
+    console.info(`[ Done 📖 Make Promptbook library ]`);
 }
 
 /**
