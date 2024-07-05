99bd6be4766237d591bbf33bc549a40e8463038e
Pavol Hejny <me@pavolhejny.com>
2024-07-05 13:34:47 +0200

    🧹 Organize imports
    🔼 This commit was automatically generated by map scripts

--- src/types/PromptbookJson/MaterialKnowledgePieceJson.ts
+++ src/types/PromptbookJson/MaterialKnowledgePieceJson.ts
@@ -1,11 +1,9 @@
 import type { string_keyword } from '../../utils/normalization/IKeywords';
-import type {
-    string_href,
-    string_markdown,
-    string_markdown_text,
-    string_model_name,
-    string_name,
-} from '../typeAliases';
+import type { string_href } from '../typeAliases';
+import type { string_markdown } from '../typeAliases';
+import type { string_markdown_text } from '../typeAliases';
+import type { string_model_name } from '../typeAliases';
+import type { string_name } from '../typeAliases';
 
 export type MaterialKnowledgePieceJson = {
     readonly name: string_name;
