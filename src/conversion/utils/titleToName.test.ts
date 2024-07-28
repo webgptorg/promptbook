9f39ff0565abc3a2dfe7d89d2ee4216e60ec4258
Pavol Hejny <me@pavolhejny.com>
2024-07-29 01:40:55 +0200

    Implement titleToName https and relative names

--- src/conversion/utils/titleToName.test.ts
+++ src/conversion/utils/titleToName.test.ts
@@ -15,11 +15,11 @@ describe('how titleToName works', () => {
 
     it('should make name url', () => {
         expect(titleToName(`https://promptbook.studio/webgpt/write-website-content.ptbk.md`)).toBe(
-            `promptbook-studio-webgpt-write-website-content`,
+            `https://promptbook.studio/webgpt/write-website-content.ptbk.md`,
         );
     });
 
     it('should make relative file path', () => {
-        expect(titleToName(`../webgpt/write-website-content.ptbk.md`)).toBe(`webgpt-write-website-content`);
+        expect(titleToName(`../webgpt/write-website-content.ptbk.md`)).toBe(`../webgpt/write-website-content.ptbk.md`);
     });
 });
--- src/conversion/utils/titleToName.ts
+++ src/conversion/utils/titleToName.ts
@@ -4,8 +4,17 @@ import { removeEmojis } from '../../utils/removeEmojis';
 /**
  * Function normalizes title to name which can be used as identifier
  */
-
 export function titleToName(value: string): string {
+    if (value.startsWith('http://') || value.startsWith('https://')) {
+        // TODO: Maybe check against some list unallowed characters
+        return value;
+    }
+
+    if (value.startsWith('./') || value.startsWith('../')) {
+        // TODO: Maybe check against some list unallowed characters
+        return value;
+    }
+
     value = removeEmojis(value);
     value = normalizeToKebabCase(value);
 
