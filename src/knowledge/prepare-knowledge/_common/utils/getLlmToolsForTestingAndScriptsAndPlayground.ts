7510a47e529d3239aec214e8a474eb704157660e
Pavol Hejny <me@pavolhejny.com>
2024-07-30 18:55:55 +0200

    Implementing createLlmToolsFromEnv

--- src/knowledge/prepare-knowledge/_common/utils/getLlmToolsForTestingAndScriptsAndPlayground.ts
+++ src/knowledge/prepare-knowledge/_common/utils/getLlmToolsForTestingAndScriptsAndPlayground.ts
@@ -1,15 +1,23 @@
 import { join } from 'path';
+import { EnvironmentMismatchError } from '../../../../errors/EnvironmentMismatchError';
 import type { LlmExecutionTools } from '../../../../execution/LlmExecutionTools';
 import { cacheLlmTools } from '../../../../llm-providers/utils/cache/cacheLlmTools';
 import { createLlmToolsFromEnv } from '../../../../llm-providers/utils/createLlmToolsFromEnv';
 import { FilesStorage } from '../../../../storage/files-storage/FilesStorage';
+import { isRunningInNode } from '../../../../utils/isRunningInWhatever';
 
 /**
  * Returns LLM tools for testing purposes
  *
- * @private within the package - JUST FOR TESTS
+ * @private within the package - JUST FOR TESTS, SCRIPTS AND PLAYGROUND
  */
 export function getLlmToolsForTestingAndScriptsAndPlayground(): LlmExecutionTools {
+    if (!isRunningInNode()) {
+        throw new EnvironmentMismatchError(
+            'Function `getLlmToolsForTestingAndScriptsAndPlayground` works only in Node.js environment',
+        );
+    }
+
     return cacheLlmTools(createLlmToolsFromEnv(), {
         storage: new FilesStorage({ cacheFolderPath: join(process.cwd(), '/executions-cache') }),
     });
--- src/llm-providers/utils/createLlmToolsFromEnv.ts
+++ src/llm-providers/utils/createLlmToolsFromEnv.ts
@@ -1,22 +1,68 @@
-import { NotYetImplementedError } from '../../errors/NotYetImplementedError';
+import spaceTrim from 'spacetrim';
+import { EnvironmentMismatchError } from '../../errors/EnvironmentMismatchError';
 import type { LlmExecutionTools } from '../../execution/LlmExecutionTools';
+import { isRunningInNode } from '../../utils/isRunningInWhatever';
+import { AnthropicClaudeExecutionTools } from '../anthropic-claude/AnthropicClaudeExecutionTools';
+import { joinLlmExecutionTools } from '../multiple/joinLlmExecutionTools';
+import { OpenAiExecutionTools } from '../openai/OpenAiExecutionTools';
 
 /**
  * @@@
  *
  * Note: This function is not cached, every call creates new instance of LlmExecutionTools
  *
+ * It looks for environment variables:
+ * - `process.env.OPENAI_API_KEY`
+ * - `process.env.ANTHROPIC_CLAUDE_API_KEY`
+ *
  * @returns @@@
  */
 export function createLlmToolsFromEnv(): LlmExecutionTools {
-  throw new NotYetImplementedError('createLlmToolsFromEnv');
+    if (!isRunningInNode()) {
+        throw new EnvironmentMismatchError('Function `createLlmToolsFromEnv` works only in Node.js environment');
+    }
+
+    const llmTools: Array<LlmExecutionTools> = [];
+
+    if (typeof process.env.OPENAI_API_KEY === 'string') {
+        llmTools.push(
+            new OpenAiExecutionTools({
+                isVerbose: true,
+                apiKey: process.env.OPENAI_API_KEY!,
+            }),
+        );
+    }
+
+    if (typeof process.env.ANTHROPIC_CLAUDE_API_KEY === 'string') {
+        llmTools.push(
+            new AnthropicClaudeExecutionTools({
+                isVerbose: true,
+                apiKey: process.env.ANTHROPIC_CLAUDE_API_KEY!,
+            }),
+        );
+    }
+
+    if (llmTools.length === 0) {
+        throw new Error(
+            spaceTrim(`
+                No LLM tools found in the environment
+
+                Please set one of environment variables:
+                - OPENAI_API_KEY
+                - ANTHROPIC_CLAUDE_API_KEY
+            `),
+        );
+    } else if (llmTools.length === 1) {
+        return llmTools[0]!;
+    } else {
+        return joinLlmExecutionTools(...llmTools);
+    }
 }
 
 /**
  * TODO: !!!!! Export this util
- * TODO: !!!!! Use this for tests in promptbook project itself
- * TODO: !!!! write discussion about this and storages
- *            write how to combine multiple interceptors
+ * TODO: !!!! write discussion about this - wizzard
+ * TODO: Add Azure
  * TODO: [🧠] Which name is better `createLlmToolsFromEnv` or `createLlmToolsFromEnvironment`?
  * TODO: [🧠] Is there some meaningfull way how to test this util
  * TODO: [🧠] Maybe pass env as argument
