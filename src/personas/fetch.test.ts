0c32803d294780a96728a3e6848ad1e12254e1fa
Pavol Hejny <me@pavolhejny.com>
2024-07-31 10:31:26 +0200

    Revert "Testing `fetch` in jest"
    This reverts commit d2bed181a044d9bb6cfee93643154964971c1cf6.

--- src/personas/fetch.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from '@jest/globals';
-
-describe('that fetch is defined', () => {
-    it('should fetch sample data', () =>
-        expect(fetch(`https://catfact.ninja/fact`).then((response) => response.json())).resolves.toEqual({
-            fact: 'A group of cats is called a “clowder.”',
-            length: 38,
-        }));
-});
