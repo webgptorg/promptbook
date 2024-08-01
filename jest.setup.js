571d35d60758d119a973518d48fba720a2ca843e
Pavol Hejny <me@pavolhejny.com>
2024-08-01 10:26:41 +0200

    Setup the environment variables for tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,4 @@
 import 'cross-fetch/polyfill';
+import * as dotenv from 'dotenv';
 
-/*
-TODO: !!!! Is there a need to setuo the environment variables for tests here?
-
-> Error: No LLM tools found in the environment
->
-> Please set one of environment variables:
-> - OPENAI_API_KEY
-> - ANTHROPIC_CLAUDE_API_KEYJest
-
-
-> process.env.FOO = 'bar';
-
-*/
+dotenv.config({ path: '.env' });
