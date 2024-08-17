1b7932787bc7d1cd79556ef8ed48a34b8225c49c
Pavol Hejny <me@pavolhejny.com>
2024-08-17 20:05:59 +0200

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/anthropic-claude.index.ts
+++ src/_packages/anthropic-claude.index.ts
@@ -1,2 +1,27 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/anthropic-claude`
+
+import { PROMPTBOOK_VERSION } from '../version';
+import { ANTHROPIC_CLAUDE_MODELS } from '../llm-providers/anthropic-claude/anthropic-claude-models';
+import { AnthropicClaudeExecutionTools } from '../llm-providers/anthropic-claude/AnthropicClaudeExecutionTools';
+import type { AnthropicClaudeExecutionToolsOptions } from '../llm-providers/anthropic-claude/AnthropicClaudeExecutionToolsOptions';
+import type { AnthropicClaudeExecutionToolsDirectOptions } from '../llm-providers/anthropic-claude/AnthropicClaudeExecutionToolsOptions';
+import type { AnthropicClaudeExecutionToolsProxiedOptions } from '../llm-providers/anthropic-claude/AnthropicClaudeExecutionToolsOptions';
+import { createAnthropicClaudeExecutionTools } from '../llm-providers/anthropic-claude/createAnthropicClaudeExecutionTools';
+import { _AnthropicClaudeRegistration } from '../llm-providers/anthropic-claude/register-constructor';
+import { _ } from '../llm-providers/anthropic-claude/register1';
+
+
+// Note: Exporting version from each package
+export { PROMPTBOOK_VERSION };
+
+
+// Note: Entities of the `@promptbook/anthropic-claude`
+export { ANTHROPIC_CLAUDE_MODELS };
+export { AnthropicClaudeExecutionTools };
+export type { AnthropicClaudeExecutionToolsOptions };
+export type { AnthropicClaudeExecutionToolsDirectOptions };
+export type { AnthropicClaudeExecutionToolsProxiedOptions };
+export { createAnthropicClaudeExecutionTools };
+export { _AnthropicClaudeRegistration };
+export { _ };
--- src/_packages/openai.index.ts
+++ src/_packages/openai.index.ts
@@ -1,2 +1,21 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/openai`
+
+import { PROMPTBOOK_VERSION } from '../version';
+import { createOpenAiExecutionTools } from '../llm-providers/openai/createOpenAiExecutionTools';
+import { OPENAI_MODELS } from '../llm-providers/openai/openai-models';
+import { OpenAiExecutionTools } from '../llm-providers/openai/OpenAiExecutionTools';
+import type { OpenAiExecutionToolsOptions } from '../llm-providers/openai/OpenAiExecutionToolsOptions';
+import { _OpenAiRegistration } from '../llm-providers/openai/register-constructor';
+
+
+// Note: Exporting version from each package
+export { PROMPTBOOK_VERSION };
+
+
+// Note: Entities of the `@promptbook/openai`
+export { createOpenAiExecutionTools };
+export { OPENAI_MODELS };
+export { OpenAiExecutionTools };
+export type { OpenAiExecutionToolsOptions };
+export { _OpenAiRegistration };
