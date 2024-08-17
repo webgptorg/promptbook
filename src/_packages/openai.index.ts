c0a0a1e8c00c39dc8004a33dc4f1d8da272ed7fa
Pavol Hejny <me@pavolhejny.com>
2024-08-17 19:55:11 +0200

    Fix `_OpenAiExecutionToolsRegistration`

--- src/_packages/openai.index.ts
+++ src/_packages/openai.index.ts
@@ -1,21 +1,2 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/openai`
-
-import { PROMPTBOOK_VERSION } from '../version';
-import { createOpenAiExecutionTools } from '../llm-providers/openai/createOpenAiExecutionTools';
-import { OPENAI_MODELS } from '../llm-providers/openai/openai-models';
-import { OpenAiExecutionTools } from '../llm-providers/openai/OpenAiExecutionTools';
-import type { OpenAiExecutionToolsOptions } from '../llm-providers/openai/OpenAiExecutionToolsOptions';
-import { _OpenAiExecutionToolsRegistration } from '../llm-providers/openai/register-constructor';
-
-
-// Note: Exporting version from each package
-export { PROMPTBOOK_VERSION };
-
-
-// Note: Entities of the `@promptbook/openai`
-export { createOpenAiExecutionTools };
-export { OPENAI_MODELS };
-export { OpenAiExecutionTools };
-export type { OpenAiExecutionToolsOptions };
-export { _OpenAiExecutionToolsRegistration };
