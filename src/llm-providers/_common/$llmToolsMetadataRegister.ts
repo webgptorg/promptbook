ce3f9152c9cb9c058b168dbfce1554e8a9b639ee
Pavol Hejny <me@pavolhejny.com>
2024-08-18 15:49:06 +0200

    $llmToolsMetadataRegister is global

--- src/llm-providers/_common/$llmToolsMetadataRegister.ts
+++ src/llm-providers/_common/$llmToolsMetadataRegister.ts
@@ -1,5 +1,20 @@
+import { $getGlobalScope } from '../../_packages/utils.index';
+import { UnexpectedError } from '../../errors/UnexpectedError';
 import { Register } from '../../utils/Register';
 import type { LlmToolsMetadata } from './LlmToolsMetadata';
+
+// TODO: !!!!!! Move this logic to Register and rename to $Register
+const globalScope = $getGlobalScope();
+
+if (globalScope.$llmToolsMetadataRegister === undefined) {
+    globalScope.$llmToolsMetadataRegister = [];
+} else if (!Array.isArray(globalScope.$llmToolsMetadataRegister)) {
+    throw new UnexpectedError(
+        `Expected $llmToolsMetadataRegister to be an array, but got ${typeof globalScope.$llmToolsMetadataRegister}`,
+    );
+}
+
+const _ = globalScope.$llmToolsMetadataRegister;
 
 /**
  * @@@
@@ -8,6 +23,6 @@ import type { LlmToolsMetadata } from './LlmToolsMetadata';
  * @singleton Only one instance of each register is created per build, but thare can be more @@@
  * @public exported from `@promptbook/core`
  */
-export const $llmToolsMetadataRegister = new Register<LlmToolsMetadata>([
-    // TODO: !!!!!! Take from global scope
-]);
+export const $llmToolsMetadataRegister = new Register<LlmToolsMetadata>(_);
+
+$getGlobalScope().$llmToolsMetadataRegister;
