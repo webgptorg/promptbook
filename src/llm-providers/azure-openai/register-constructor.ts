e58c5e9d7fd9d3f2e15d86fa6237b09bcd3a4565
Pavol Hejny <me@pavolhejny.com>
2024-08-18 17:53:18 +0200

    `createAzureOpenAiExecutionTools`

---
+++ src/llm-providers/azure-openai/register-constructor.ts
@@ -0,0 +1,15 @@
+import { $llmToolsRegister } from '../_common/$llmToolsRegister';
+import { createAzureOpenAiExecutionTools } from './createAzureOpenAiExecutionTools';
+
+/**
+ * @@@ registration2
+ *
+ * Note: [🏐] Configurations registrations are done in @@@ BUT constructor @@@
+ *
+ * @public exported from `@promptbook/azure-openai`
+ */
+export const _AzureOpenAiRegistration = $llmToolsRegister.register(createAzureOpenAiExecutionTools);
+
+/**
+ * TODO: [🎶] Naming "constructor" vs "creator" vs "factory"
+ */
