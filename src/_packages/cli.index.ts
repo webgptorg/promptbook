587073319f6f30630e704155d04f54e333a48105
Pavol Hejny <me@pavolhejny.com>
2024-08-18 10:47:16 +0200

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/cli.index.ts
+++ src/_packages/cli.index.ts
@@ -3,6 +3,8 @@
 
 import { PROMPTBOOK_VERSION } from '../version';
 import { __CLI } from '../cli/main';
+import { _AnthropicClaudeMetadataRegistration } from '../llm-providers/anthropic-claude/register-configuration';
+import { _OpenAiMetadataRegistration } from '../llm-providers/openai/register-configuration';
 
 
 // Note: Exporting version from each package
@@ -11,3 +13,5 @@ export { PROMPTBOOK_VERSION };
 
 // Note: Entities of the `@promptbook/cli`
 export { __CLI };
+export { _AnthropicClaudeMetadataRegistration };
+export { _OpenAiMetadataRegistration };
