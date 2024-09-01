839c9bed3e19b499fc732005de5e86535d830185
Pavol Hejny <me@pavolhejny.com>
2024-09-01 17:50:52 +0200

    🧹 Repair imports
    🔼 This commit was automatically generated by map scripts

--- src/commands/PERSONA/personaCommandParser.ts
+++ src/commands/PERSONA/personaCommandParser.ts
@@ -4,12 +4,10 @@ import { ParseError } from '../../errors/ParseError';
 import type { PipelineJson } from '../../types/PipelineJson/PipelineJson';
 import type { string_markdown_text } from '../../types/typeAliases';
 import { keepUnused } from '../../utils/organization/keepUnused';
-import type {
-    $PipelineJson,
-    $TemplateJson,
-    CommandParserInput,
-    PipelineBothCommandParser,
-} from '../_common/types/CommandParser';
+import type { $PipelineJson } from '../_common/types/CommandParser';
+import type { $TemplateJson } from '../_common/types/CommandParser';
+import type { CommandParserInput } from '../_common/types/CommandParser';
+import type { PipelineBothCommandParser } from '../_common/types/CommandParser';
 import type { PersonaCommand } from './PersonaCommand';
 
 /**
