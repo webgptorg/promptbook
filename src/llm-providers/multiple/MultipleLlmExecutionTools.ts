f14a6b6fad68b5173ed27a43bebf010bb81fcf3d
Pavol Hejny <me@pavolhejny.com>
2024-09-10 15:52:05 +0200

    Better name MultipleLlmExecutionTools

--- src/llm-providers/multiple/MultipleLlmExecutionTools.ts
+++ src/llm-providers/multiple/MultipleLlmExecutionTools.ts
@@ -3,17 +3,14 @@ import { PipelineExecutionError } from '../../errors/PipelineExecutionError';
 import { UnexpectedError } from '../../errors/UnexpectedError';
 import type { AvailableModel } from '../../execution/AvailableModel';
 import type { LlmExecutionTools } from '../../execution/LlmExecutionTools';
-import type { ChatPromptResult } from '../../execution/PromptResult';
-import type { CompletionPromptResult } from '../../execution/PromptResult';
-import type { EmbeddingPromptResult } from '../../execution/PromptResult';
-import type { PromptResult } from '../../execution/PromptResult';
-import type { ChatPrompt } from '../../types/Prompt';
-import type { CompletionPrompt } from '../../types/Prompt';
-import type { EmbeddingPrompt } from '../../types/Prompt';
-import type { Prompt } from '../../types/Prompt';
-import type { string_markdown } from '../../types/typeAliases';
-import type { string_markdown_text } from '../../types/typeAliases';
-import type { string_title } from '../../types/typeAliases';
+import type {
+    ChatPromptResult,
+    CompletionPromptResult,
+    EmbeddingPromptResult,
+    PromptResult,
+} from '../../execution/PromptResult';
+import type { ChatPrompt, CompletionPrompt, EmbeddingPrompt, Prompt } from '../../types/Prompt';
+import type { string_markdown, string_markdown_text, string_title } from '../../types/typeAliases';
 import type { really_any } from '../../utils/organization/really_any';
 
 /**
@@ -41,9 +38,7 @@ export class MultipleLlmExecutionTools implements LlmExecutionTools {
     }
 
     public get description(): string_markdown {
-        return this.llmExecutionTools
-            .map((tools, index) => `${index + 1}) ${tools.title} ${tools.description || ''}`)
-            .join('\n');
+        return this.llmExecutionTools.map(({ title }, index) => `${index + 1}) \`${title}\``).join('\n');
     }
 
     /**
@@ -177,7 +172,7 @@ export class MultipleLlmExecutionTools implements LlmExecutionTools {
                           }"
 
                           Available \`LlmExecutionTools\`:
-                          ${block(this.llmExecutionTools.map((tools) => `- ${tools.title}`).join('\n'))}
+                          ${block(this.description)}
 
                     `,
                 ),
