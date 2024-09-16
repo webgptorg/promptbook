3a0557fb79fa0fa3a6e8eb938f03624d02d7b221
Pavol Hejny <me@pavolhejny.com>
2024-09-16 16:58:07 +0200

    🧹 Repair imports
    🔼 This commit was automatically generated by map scripts

--- src/execution/createPipelineExecutor/$OngoingTemplateResult.ts
+++ src/execution/createPipelineExecutor/$OngoingTemplateResult.ts
@@ -1,6 +1,9 @@
 import { ExpectError } from '../../errors/ExpectError';
 import type { Prompt } from '../../types/Prompt';
-import type { ChatPromptResult, CompletionPromptResult, EmbeddingPromptResult, PromptResult } from '../PromptResult';
+import type { ChatPromptResult } from '../PromptResult';
+import type { CompletionPromptResult } from '../PromptResult';
+import type { EmbeddingPromptResult } from '../PromptResult';
+import type { PromptResult } from '../PromptResult';
 
 /**
  * @@@
@@ -13,39 +16,38 @@ export type $OngoingTemplateResult = {
      */
     $prompt?: Prompt;
 
-        /**
+    /**
      * @@@
      */
     $chatResult?: ChatPromptResult;
 
-        /**
+    /**
      * @@@
      */
     $completionResult?: CompletionPromptResult;
 
-        /**
+    /**
      * @@@
      */
     $embeddingResult?: EmbeddingPromptResult;
     //  <- Note: [🤖]
 
-
-        /**
+    /**
      * @@@
      */
     $result: PromptResult | null;
 
-        /**
+    /**
      * @@@
      */
     $resultString: string | null;
 
-        /**
+    /**
      * @@@
      */
     $expectError: ExpectError | null;
 
-        /**
+    /**
      * @@@
      */
     $scriptPipelineExecutionErrors: Array<Error>;
