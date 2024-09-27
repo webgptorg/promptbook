b2f74ea93e11caae739ee5126417ac3236bab745
Pavol Hejny <me@pavolhejny.com>
2024-09-27 15:27:30 +0200

    Fix lint

--- src/utils/files/getFileExtension.ts
+++ src/utils/files/getFileExtension.ts
@@ -6,6 +6,6 @@ import type { string_file_path, string_mime_type } from '../../types/typeAliases
  * @private within the repository
  */
 export function getFileExtension(value: string_file_path): string_mime_type | null {
-    const match = value.match(/\.([0-9a-z]+)(?:[\?#]|$)/i);
+    const match = value.match(/\.([0-9a-z]+)(?:[?#]|$)/i);
     return match ? match[1]!.toLowerCase() : null;
 }
