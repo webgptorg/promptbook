642d8554b6593be32c846579dd9a3d460a7c38c7
Pavol Hejny <me@pavolhejny.com>
2024-10-02 14:59:47 +0200

    Types

--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -1,19 +1,22 @@
+import { Scraper } from './_common/Scraper';
 import { legacyDocumentScraper } from './document-legacy/legacyDocumentScraper';
 import { documentScraper } from './document/documentScraper';
 import { markdownScraper } from './markdown/markdownScraper';
 import { pdfScraper } from './pdf/pdfScraper';
 import { websiteScraper } from './website/websiteScraper';
 
+// TODO: [🦖] !!!!!! Pass scrapers as dependency,
+
 /**
  * @@@
  *
  * @public exported from `@promptbook/core`
  */
-export const SCRAPERS = [
+export const SCRAPERS: Array<Scraper> = [
     markdownScraper,
     documentScraper,
     legacyDocumentScraper,
     pdfScraper,
     websiteScraper,
     // <- Note: [♓️] This is the order of the scrapers for knowledge, BUT consider some better (more explicit) way to do this
-] as const;
+];
