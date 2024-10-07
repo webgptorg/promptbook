d6f7f882e291063d236e6016bcff06b80c12c23c
Pavol Hejny <me@pavolhejny.com>
2024-10-07 17:11:12 +0200

    🆚 Update version in config
    🔼 This commit was automatically generated by map scripts

--- src/version.ts
+++ src/version.ts
@@ -5,7 +5,7 @@ import type { string_semantic_version } from './types/typeAliases';
 /**
  * The version of the Promptbook library
  */
-export const PROMPTBOOK_VERSION: string_promptbook_version = '0.71.0-0';
+export const PROMPTBOOK_VERSION: string_promptbook_version = '0.71.0-2';
 
 export type string_promptbook_version = string_semantic_version;
 // TODO: [main] !!!! List here all the versions and annotate + put into script
