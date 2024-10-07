6d29578ba19f042d49c763db5a17507e20b545af
Pavol Hejny <me@pavolhejny.com>
2024-10-07 23:56:48 +0200

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/browser.index.ts
+++ src/_packages/browser.index.ts
@@ -1,13 +1,17 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/browser`
 
+import { PROMPTBOOK_VERSION } from '../version';
 import { SimplePromptInterfaceTools } from '../dialogs/simple-prompt/SimplePromptInterfaceTools';
 import { getLocalStorage } from '../storage/local-storage/getLocalStorage';
 import { getSessionStorage } from '../storage/local-storage/getSessionStorage';
-import { PROMPTBOOK_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { PROMPTBOOK_VERSION };
 
+
 // Note: Entities of the `@promptbook/browser`
-export { getLocalStorage, getSessionStorage, SimplePromptInterfaceTools };
+export { SimplePromptInterfaceTools };
+export { getLocalStorage };
+export { getSessionStorage };
--- src/_packages/node.index.ts
+++ src/_packages/node.index.ts
@@ -1,19 +1,23 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/node`
 
+import { PROMPTBOOK_VERSION } from '../version';
 import { createCollectionFromDirectory } from '../collection/constructors/createCollectionFromDirectory';
 import { createLlmToolsFromConfigurationFromEnv } from '../llm-providers/_common/createLlmToolsFromConfigurationFromEnv';
 import { createLlmToolsFromEnv } from '../llm-providers/_common/createLlmToolsFromEnv';
 import { FileCacheStorage } from '../storage/file-cache-storage/FileCacheStorage';
-import { PROMPTBOOK_VERSION } from '../version';
+import { $execCommand } from '../utils/execCommand/$execCommand';
+import { $execCommands } from '../utils/execCommand/$execCommands';
+
 
 // Note: Exporting version from each package
 export { PROMPTBOOK_VERSION };
 
+
 // Note: Entities of the `@promptbook/node`
-export {
-    createCollectionFromDirectory,
-    createLlmToolsFromConfigurationFromEnv,
-    createLlmToolsFromEnv,
-    FileCacheStorage,
-};
+export { createCollectionFromDirectory };
+export { createLlmToolsFromConfigurationFromEnv };
+export { createLlmToolsFromEnv };
+export { FileCacheStorage };
+export { $execCommand };
+export { $execCommands };
