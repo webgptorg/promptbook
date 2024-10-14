62c9b64abeb0b8728b38e55a2f22a9d561d4397f
Pavol Hejny <me@pavolhejny.com>
2024-10-14 23:52:04 +0200

    Making scrapers Classy

--- src/scrapers/markdown/markdownScraper-.test.ts
+++ src/scrapers/markdown/markdownScraper-.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from '@jest/globals';
 import { join } from 'path';
 import { getLlmToolsForTestingAndScriptsAndPlayground } from '../../llm-providers/_common/register/getLlmToolsForTestingAndScriptsAndPlayground';
 import { makeKnowledgeSourceHandler } from '../_common/utils/makeKnowledgeSourceHandler';
-import { MarkdownScraper } from './MarkdownScraper-';
+import { MarkdownScraper } from './MarkdownScraper';
 
 describe('how creating knowledge from markdown works', () => {
     const rootDirname = join(__dirname, 'samples');
