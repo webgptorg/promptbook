96f65e332b0e71b2a61e4b63c0357f81309b5c39
Pavol Hejny <me@pavolhejny.com>
2024-10-16 23:35:56 +0200

    Revert "Remove factory functions for scrapers"
    This reverts commit 6b9ceb41fdf281cbbd001b6348438edd4db7e7a8.

---
+++ src/scrapers/website/createWebsiteScraper.ts
@@ -0,0 +1,22 @@
+import { ScraperConstructor } from '../_common/ScraperConstructor';
+import { WebsiteScraperOptions } from './WebsiteScraperOptions';
+
+/**
+ * @@@
+ *
+ * @public exported from `@promptbook/website-crawler`
+ */
+export const createWebsiteScraper = Object.assign(
+    (options: WebsiteScraperOptions): WebsiteScraper => {
+        return new WebsiteScraper(options);
+    },
+    {
+        packageName: '@promptbook/website-crawler',
+        className: 'WebsiteScraper',
+    },
+) satisfies ScraperConstructor;
+
+/**
+ * TODO: [🦺] Is there some way how to put `packageName` and `className` on top and function definition on bottom?
+ * TODO: [🎶] Naming "constructor" vs "creator" vs "factory"
+ */
