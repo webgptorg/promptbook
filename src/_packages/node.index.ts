d20738d3889d93db5ee6456b8034d3a9bc7ef7ce
Pavol Hejny <me@pavolhejny.com>
2024-10-16 19:07:10 +0200

    Generate packages

--- src/_packages/node.index.ts
+++ src/_packages/node.index.ts
@@ -1,27 +1,27 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/node`
 
+import { PROMPTBOOK_VERSION } from '../version';
 import { createCollectionFromDirectory } from '../collection/constructors/createCollectionFromDirectory';
+import { $provideExecutionToolsForNode } from '../execution/utils/$provideExecutionToolsForNode';
 import { $provideLlmToolsConfigurationFromEnv } from '../llm-providers/_common/register/$provideLlmToolsConfigurationFromEnv';
 import { $provideLlmToolsFromEnv } from '../llm-providers/_common/register/$provideLlmToolsFromEnv';
-import { createScrapersFromConfigurationFromEnv } from '../scrapers/_common/register/createScrapersFromConfigurationFromEnv';
-import { createScrapersFromEnv } from '../scrapers/_common/register/createScrapersFromEnv';
+import { $provideScrapersForNode } from '../scrapers/_common/register/$provideScrapersForNode';
 import { FileCacheStorage } from '../storage/file-cache-storage/FileCacheStorage';
 import { $execCommand } from '../utils/execCommand/$execCommand';
 import { $execCommands } from '../utils/execCommand/$execCommands';
-import { PROMPTBOOK_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { PROMPTBOOK_VERSION };
 
+
 // Note: Entities of the `@promptbook/node`
-export {
-    $execCommand,
-    $execCommands,
-    $provideLlmToolsConfigurationFromEnv,
-    $provideLlmToolsFromEnv,
-    createCollectionFromDirectory,
-    createScrapersFromConfigurationFromEnv,
-    createScrapersFromEnv,
-    FileCacheStorage,
-};
+export { createCollectionFromDirectory };
+export { $provideExecutionToolsForNode };
+export { $provideLlmToolsConfigurationFromEnv };
+export { $provideLlmToolsFromEnv };
+export { $provideScrapersForNode };
+export { FileCacheStorage };
+export { $execCommand };
+export { $execCommands };
--- src/_packages/website-crawler.index.ts
+++ src/_packages/website-crawler.index.ts
@@ -1,13 +1,17 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/website-crawler`
 
+import { PROMPTBOOK_VERSION } from '../version';
 import { createWebsiteScraper } from '../scrapers/website/createWebsiteScraper';
 import { _WebsiteScraperRegistration } from '../scrapers/website/register-constructor';
 import { WebsiteScraper } from '../scrapers/website/WebsiteScraper';
-import { PROMPTBOOK_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { PROMPTBOOK_VERSION };
 
+
 // Note: Entities of the `@promptbook/website-crawler`
-export { _WebsiteScraperRegistration, createWebsiteScraper, WebsiteScraper };
+export { createWebsiteScraper };
+export { _WebsiteScraperRegistration };
+export { WebsiteScraper };
