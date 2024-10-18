dbe02836f90eeee74819d8fb05d62680a959fac5
Pavol Hejny <me@pavolhejny.com>
2024-10-18 22:14:54 +0200

    Generate packages

--- src/_packages/cli.index.ts
+++ src/_packages/cli.index.ts
@@ -11,6 +11,16 @@ import { _OpenAiMetadataRegistration } from '../llm-providers/openai/register-co
 import { _OpenAiAssistantMetadataRegistration } from '../llm-providers/openai/register-configuration';
 import { _OpenAiRegistration } from '../llm-providers/openai/register-constructor';
 import { _OpenAiAssistantRegistration } from '../llm-providers/openai/register-constructor';
+import { _LegacyDocumentScraperRegistration } from '../scrapers/document-legacy/register-constructor';
+import { _LegacyDocumentScraperMetadataRegistration } from '../scrapers/document-legacy/register-metadata';
+import { _DocumentScraperRegistration } from '../scrapers/document/register-constructor';
+import { _DocumentScraperMetadataRegistration } from '../scrapers/document/register-metadata';
+import { _MarkdownScraperRegistration } from '../scrapers/markdown/register-constructor';
+import { _MarkdownScraperMetadataRegistration } from '../scrapers/markdown/register-metadata';
+import { _PdfScraperRegistration } from '../scrapers/pdf/register-constructor';
+import { _PdfScraperMetadataRegistration } from '../scrapers/pdf/register-metadata';
+import { _WebsiteScraperRegistration } from '../scrapers/website/register-constructor';
+import { _WebsiteScraperMetadataRegistration } from '../scrapers/website/register-metadata';
 
 
 // Note: Exporting version from each package
@@ -27,3 +37,13 @@ export { _OpenAiMetadataRegistration };
 export { _OpenAiAssistantMetadataRegistration };
 export { _OpenAiRegistration };
 export { _OpenAiAssistantRegistration };
+export { _LegacyDocumentScraperRegistration };
+export { _LegacyDocumentScraperMetadataRegistration };
+export { _DocumentScraperRegistration };
+export { _DocumentScraperMetadataRegistration };
+export { _MarkdownScraperRegistration };
+export { _MarkdownScraperMetadataRegistration };
+export { _PdfScraperRegistration };
+export { _PdfScraperMetadataRegistration };
+export { _WebsiteScraperRegistration };
+export { _WebsiteScraperMetadataRegistration };
