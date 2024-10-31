c01219fbc2afa75a6cab639a310d32cdb992cdf1
Pavol Hejny <me@pavolhejny.com>
2024-10-31 08:33:11 +0100

    Revert "Try to fix build problem - temporary remove of `@promptbook/website-crawler`"
    This reverts commit 6e377e4a8b3e0dbadf8f14371be0b7b74dc82060.

--- src/_packages/cli.index.ts
+++ src/_packages/cli.index.ts
@@ -1,16 +1,16 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/cli`
 
-import { PROMPTBOOK_VERSION } from '../version';
 import { _CLI } from '../cli/main';
 import { _AnthropicClaudeMetadataRegistration } from '../llm-providers/anthropic-claude/register-configuration';
 import { _AnthropicClaudeRegistration } from '../llm-providers/anthropic-claude/register-constructor';
 import { _AzureOpenAiMetadataRegistration } from '../llm-providers/azure-openai/register-configuration';
 import { _AzureOpenAiRegistration } from '../llm-providers/azure-openai/register-constructor';
-import { _OpenAiMetadataRegistration } from '../llm-providers/openai/register-configuration';
-import { _OpenAiAssistantMetadataRegistration } from '../llm-providers/openai/register-configuration';
-import { _OpenAiRegistration } from '../llm-providers/openai/register-constructor';
-import { _OpenAiAssistantRegistration } from '../llm-providers/openai/register-constructor';
+import {
+    _OpenAiAssistantMetadataRegistration,
+    _OpenAiMetadataRegistration,
+} from '../llm-providers/openai/register-configuration';
+import { _OpenAiAssistantRegistration, _OpenAiRegistration } from '../llm-providers/openai/register-constructor';
 import { _LegacyDocumentScraperRegistration } from '../scrapers/document-legacy/register-constructor';
 import { _LegacyDocumentScraperMetadataRegistration } from '../scrapers/document-legacy/register-metadata';
 import { _DocumentScraperRegistration } from '../scrapers/document/register-constructor';
@@ -19,27 +19,32 @@ import { _MarkdownScraperRegistration } from '../scrapers/markdown/register-cons
 import { _MarkdownScraperMetadataRegistration } from '../scrapers/markdown/register-metadata';
 import { _PdfScraperRegistration } from '../scrapers/pdf/register-constructor';
 import { _PdfScraperMetadataRegistration } from '../scrapers/pdf/register-metadata';
-
+import { _WebsiteScraperRegistration } from '../scrapers/website/register-constructor';
+import { _WebsiteScraperMetadataRegistration } from '../scrapers/website/register-metadata';
+import { PROMPTBOOK_VERSION } from '../version';
 
 // Note: Exporting version from each package
 export { PROMPTBOOK_VERSION };
 
-
 // Note: Entities of the `@promptbook/cli`
-export { _CLI };
-export { _AnthropicClaudeMetadataRegistration };
-export { _AnthropicClaudeRegistration };
-export { _AzureOpenAiMetadataRegistration };
-export { _AzureOpenAiRegistration };
-export { _OpenAiMetadataRegistration };
-export { _OpenAiAssistantMetadataRegistration };
-export { _OpenAiRegistration };
-export { _OpenAiAssistantRegistration };
-export { _LegacyDocumentScraperRegistration };
-export { _LegacyDocumentScraperMetadataRegistration };
-export { _DocumentScraperRegistration };
-export { _DocumentScraperMetadataRegistration };
-export { _MarkdownScraperRegistration };
-export { _MarkdownScraperMetadataRegistration };
-export { _PdfScraperRegistration };
-export { _PdfScraperMetadataRegistration };
+export {
+    _AnthropicClaudeMetadataRegistration,
+    _AnthropicClaudeRegistration,
+    _AzureOpenAiMetadataRegistration,
+    _AzureOpenAiRegistration,
+    _CLI,
+    _DocumentScraperMetadataRegistration,
+    _DocumentScraperRegistration,
+    _LegacyDocumentScraperMetadataRegistration,
+    _LegacyDocumentScraperRegistration,
+    _MarkdownScraperMetadataRegistration,
+    _MarkdownScraperRegistration,
+    _OpenAiAssistantMetadataRegistration,
+    _OpenAiAssistantRegistration,
+    _OpenAiMetadataRegistration,
+    _OpenAiRegistration,
+    _PdfScraperMetadataRegistration,
+    _PdfScraperRegistration,
+    _WebsiteScraperMetadataRegistration,
+    _WebsiteScraperRegistration,
+};
