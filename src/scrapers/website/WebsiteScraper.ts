6df1e82a564739a69b5c1f6c1eec2e1644758dbc
Pavol Hejny <me@pavolhejny.com>
2024-11-04 21:09:23 +0100

    🧹 Repair imports
    🔼 This commit was automatically generated by map scripts

--- src/scrapers/website/WebsiteScraper.ts
+++ src/scrapers/website/WebsiteScraper.ts
@@ -1,7 +1,8 @@
 import type { KnowledgePiecePreparedJson } from '../../types/PipelineJson/KnowledgePieceJson';
 import type { string_markdown } from '../../types/typeAliases';
 import type { Converter } from '../_common/Converter';
-import type { Scraper, ScraperSourceHandler } from '../_common/Scraper';
+import type { Scraper } from '../_common/Scraper';
+import type { ScraperSourceHandler } from '../_common/Scraper';
 // TODO: [🏳‍🌈] Finally take pick of .json vs .ts
 // import PipelineCollection from '../../../promptbook-collection/promptbook-collection';
 import { Readability } from '@mozilla/readability';
