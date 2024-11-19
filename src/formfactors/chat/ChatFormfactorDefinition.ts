6d3c678ef716b824779c7aa26fffbee3fd6b8835
Pavol Hejny <me@pavolhejny.com>
2024-11-19 18:01:48 +0100

    TODOs

--- src/formfactors/chat/ChatFormfactorDefinition.ts
+++ src/formfactors/chat/ChatFormfactorDefinition.ts
@@ -13,5 +13,16 @@ export const ChatFormfactorDefinition = {
     pipelineInterface: {
         inputParameterNames: ['previousTitle', 'previousConversationSummary', 'userMessage'],
         outputParameterNames: ['title', 'conversationSummary', 'chatbotResponse'],
+        /*
+        <- TODO: !!!!!! Change to 
+
+        -   INPUT PARAMETER `{previousTitle}` Previous title of the conversation
+        -   INPUT PARAMETER `{previousConversationSummary}` Previous conversation summary
+        -   INPUT PARAMETER `{userMessage}` User message
+        -   OUTPUT PARAMETER `{title}` Title of the conversation
+        -   OUTPUT PARAMETER `{conversationSummary}` Summary of the conversation
+        -   OUTPUT PARAMETER `{chatbotResponse}` Chatbot response
+
+        */
     },
 } as const satisfies AbstractFormfactorDefinition;
