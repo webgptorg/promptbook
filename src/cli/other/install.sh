f32e9afc23d46f03db4c9235464852dbfea034b8
Pavol Hejny <me@pavolhejny.com>
2024-11-26 13:46:12 +0100

    TODOs

--- src/cli/other/install.sh
+++ src/cli/other/install.sh
@@ -3,7 +3,9 @@
 # TODO: [🎺] This is a draft of installer script
 
 # Look at real installer script and take inspiration from it
+# @see https://github.com/nvm-sh/nvm/blob/master/README.md
 #> curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+#> wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 cd /usr/bin
 
