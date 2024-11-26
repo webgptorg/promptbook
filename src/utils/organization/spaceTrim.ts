6e6e29a07cd8c3b814eb3b75f9e7f60714a18794
Pavol Hejny <me@pavolhejny.com>
2024-11-26 15:09:34 +0100

    Reexport spacetrim

---
+++ src/utils/organization/spaceTrim.ts
@@ -0,0 +1,11 @@
+import { spaceTrim as spaceTrimReexported } from 'spacetrim';
+
+/**
+ * Trims string from all 4 sides
+ *
+ * Note: This is a re-exported function from the `spacetrim` package which is
+ *       Developed by same author @hejny as this package
+ *
+ * @see https://github.com/hejny/spacetrim#usage
+ */
+export const spaceTrim = spaceTrimReexported;
