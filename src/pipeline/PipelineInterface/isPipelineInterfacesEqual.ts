8c8bc0f2f228d3a2873e4bfa70756ab0fc63d3a7
Pavol Hejny <me@pavolhejny.com>
2024-11-29 10:13:16 +0100

    Making provisional pipeline interfaces until https://github.com/webgptorg/promptbook/pull/186

--- src/pipeline/PipelineInterface/isPipelineInterfacesEqual.ts
+++ src/pipeline/PipelineInterface/isPipelineInterfacesEqual.ts
@@ -2,7 +2,7 @@ import type { PipelineInterface } from './PipelineInterface';
 
 /**
  * @@@
- * 
+ *
  * @deprecated https://github.com/webgptorg/promptbook/pull/186
  * @see https://github.com/webgptorg/promptbook/discussions/171
  *
@@ -12,6 +12,32 @@ export function isPipelineInterfacesEqual(
     pipelineInterface1: PipelineInterface,
     pipelineInterface2: PipelineInterface,
 ): boolean {
-    return JSON.stringify(Object.keys(pipelineInterface1)) === JSON.stringify(Object.keys(pipelineInterface2));
-}
+    for (const whichParameters of ['inputParameters', 'outputParameters'] as const) {
+        const parameters1 = pipelineInterface1[whichParameters];
+        const parameters2 = pipelineInterface2[whichParameters];
 
+        if (parameters1.length !== parameters2.length) {
+            return false;
+        }
+
+        for (const parameter of parameters1) {
+            const matchingParameter = parameters2.find(({ name }) => name === parameter.name);
+
+            if (!matchingParameter) {
+                return false;
+            }
+
+            // Note: Do not compare description, it is not relevant for compatibility
+
+            if (matchingParameter.isInput !== parameter.isInput) {
+                return false;
+            }
+
+            if (matchingParameter.isOutput !== parameter.isOutput) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
