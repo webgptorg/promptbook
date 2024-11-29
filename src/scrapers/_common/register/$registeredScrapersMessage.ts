a5ba8c8968e2227090803bf2e890bf424746be5c
Pavol Hejny <me@pavolhejny.com>
2024-11-29 14:39:20 +0100

    🧹 Repair imports
    🔼 This commit was automatically generated by map scripts

--- src/scrapers/_common/register/$registeredScrapersMessage.ts
+++ src/scrapers/_common/register/$registeredScrapersMessage.ts
@@ -1,6 +1,7 @@
 import spaceTrim from 'spacetrim';
-import type { string_markdown, string_markdown_text } from '../../../types/typeAliases';
-import { Scraper } from '../Scraper';
+import type { string_markdown } from '../../../types/typeAliases';
+import type { string_markdown_text } from '../../../types/typeAliases';
+import type { Scraper } from '../Scraper';
 import { $scrapersMetadataRegister } from './$scrapersMetadataRegister';
 import { $scrapersRegister } from './$scrapersRegister';
 import type { ScraperAndConverterMetadata } from './ScraperAndConverterMetadata';
