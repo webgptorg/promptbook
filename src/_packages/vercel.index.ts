15c716195c287a8cc5c8548c5167fd61c2de8d5c
Pavol Hejny <me@pavolhejny.com>
2024-12-07 19:56:15 +0100

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/vercel.index.ts
+++ src/_packages/vercel.index.ts
@@ -2,7 +2,7 @@
 // `@promptbook/vercel`
 
 import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
-
+import { createExecutionToolsFromVercelProvider } from '../llm-providers/vercel/createExecutionToolsFromVercelProvider';
 
 
 // Note: Exporting version from each package
@@ -10,3 +10,4 @@ export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
 
 // Note: Entities of the `@promptbook/vercel`
+export { createExecutionToolsFromVercelProvider };
