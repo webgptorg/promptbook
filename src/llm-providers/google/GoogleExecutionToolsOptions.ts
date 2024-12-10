4b1fd405361731d8a8e7dbbf8890fe4456672d38
Pavol Hejny <me@pavolhejny.com>
2024-12-10 13:34:50 +0100

    Betetr `GoogleExecutionToolsOptions`

--- src/llm-providers/google/GoogleExecutionToolsOptions.ts
+++ src/llm-providers/google/GoogleExecutionToolsOptions.ts
@@ -1,15 +1,11 @@
 import type { createGoogleGenerativeAI } from '@ai-sdk/google';
-import type { CommonToolsOptions } from '../../execution/CommonToolsOptions';
+import { VercelExecutionToolsOptions } from '../vercel/VercelExecutionToolsOptions';
 
 /**
  * Options for `GoogleExecutionTools`
  *
- * !!!!!! This extends Google's `ClientOptions` with are directly passed to the Google generative AI client.
+ * This combines options for Promptbook, Google and Vercel together
  * @public exported from `@promptbook/google`
  */
-export type GoogleExecutionToolsOptions = CommonToolsOptions &
-    Parameters<typeof createGoogleGenerativeAI>[0] /* TODO: !!!!!! & ClientOptions*/;
-
-/**
- * TODO: [🧠][🤺] Pass `userId`
- */
+export type GoogleExecutionToolsOptions = Omit<VercelExecutionToolsOptions, 'vercelProvider' | 'availableModels'> &
+    Parameters<typeof createGoogleGenerativeAI>[0];
