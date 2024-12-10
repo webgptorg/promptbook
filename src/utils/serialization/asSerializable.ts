8f689a8bf6ba00155ac6bb7edf91f848c18f8a0a
Pavol Hejny <me@pavolhejny.com>
2024-12-10 14:03:07 +0100

    Serialize raw responses

--- src/utils/serialization/asSerializable.ts
+++ src/utils/serialization/asSerializable.ts
@@ -1,7 +1,9 @@
+import { really_any } from '../organization/really_any';
 import { really_unknown } from '../organization/really_unknown';
 
 /**
- * Function `asSerializable` will convert values which are not serializable to serializable values.
+ * Function `asSerializable` will convert values which are not serializable to serializable values
+ * It walks deeply through the object and converts all values
  *
  * For example:
  * - `Date` objects will be converted to string
@@ -9,4 +11,20 @@ import { really_unknown } from '../organization/really_unknown';
  * @private Internal helper function
  */
 
-export function asSerializable(value: really_unknown): really_unknown {}
+export function asSerializable(value: really_any): really_any {
+    if (value instanceof Date) {
+        return value.toISOString();
+    } else if (Array.isArray(value)) {
+        return value.map(asSerializable);
+    } else if (value !== null && typeof value === 'object') {
+        const result: really_unknown = {};
+        for (const key in value) {
+            if (Object.prototype.hasOwnProperty.call(value, key)) {
+                (result as really_any)[key] = asSerializable(value[key]);
+            }
+        }
+        return result;
+    } else {
+        return value;
+    }
+}
