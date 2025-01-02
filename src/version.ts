93ac0f8b5388d62566cb01e392506cc7eba1d756
Pavol Hejny <me@pavolhejny.com>
2025-01-02 16:48:16 +0100

    🆚 Update version in config
    🔼 This commit was automatically generated by map scripts

--- src/version.ts
+++ src/version.ts
@@ -5,6 +5,7 @@ import type { string_semantic_version } from './types/typeAliases';
 /**
  * The version of the Book language
  *
+ * @generated
  * @see https://github.com/webgptorg/book
  */
 export const BOOK_LANGUAGE_VERSION: string_semantic_version = '1.0.0';
@@ -12,10 +13,17 @@ export const BOOK_LANGUAGE_VERSION: string_semantic_version = '1.0.0';
 /**
  * The version of the Promptbook engine
  *
+ * @generated
  * @see https://github.com/webgptorg/promptbook
  */
-export const PROMPTBOOK_ENGINE_VERSION: string_promptbook_version = '0.81.0-5';
+export const PROMPTBOOK_ENGINE_VERSION: string_promptbook_version = '0.81.0-6';
 
+
+/**
+ * @@@
+ *
+ * @generated
+ */
 export type string_promptbook_version = string_semantic_version;
 
 /**
