85bdb2114c11c244e6c72409e0e821a3eb8d4522
Pavol Hejny <me@pavolhejny.com>
2025-01-03 16:30:13 +0100

    `getBookTemplate` is working

--- src/other/templates/getBookTemplate.ts
+++ src/other/templates/getBookTemplate.ts
@@ -1,35 +1,40 @@
+import { PipelineCollection } from '../../collection/PipelineCollection';
+import { UnexpectedError } from '../../errors/UnexpectedError';
 import type { string_formfactor_name } from '../../formfactors/_common/string_formfactor_name';
 import type { PipelineJson } from '../../pipeline/PipelineJson/PipelineJson';
-import type { TODO_any } from '../../utils/organization/TODO_any';
 import { getTemplatesPipelineCollection } from './getTemplatesPipelineCollection';
+
+/**
+ * @@@
+ *
+ * @singleton
+ * @private internal cache of `getBookTemplate`
+ */
+export let templatesPipelineCollection: PipelineCollection | null = null;
 
 /**
  * Get template for new book
  *
  * @public exported from `@promptbook/templates`
  */
-export function getBookTemplate(formfactorName: string_formfactor_name): PipelineJson | null {
-    const templatesPipelineCollection = getTemplatesPipelineCollection();
+export function getBookTemplate(formfactorName: string_formfactor_name): PipelineJson {
+    if (templatesPipelineCollection === null) {
+        templatesPipelineCollection = getTemplatesPipelineCollection();
+    }
 
-    const pipelineJson =
-        (
-            {
-                templatesPipelineCollection,
+    const pipelineJson = templatesPipelineCollection.getPipelineByUrl(
+        `https://github.com/webgptorg/book/blob/main/books/templates/${formfactorName}.book.md`,
+    ) as PipelineJson; // <- Note: !!!!!! `SimplePipelineCollection`
 
-                // TODO: !!!!!! Add other formfactors when they are ready
-                // chatbotBook,
-                // translatorBook,
-                // sheetsBook,
-                // generatorBook,
-            } as TODO_any
-        ) /* TODO: as Record<`${Lowercase<string_formfactor_name>}Book`, PipelineString> */[
-            `${formfactorName.toLowerCase()}Book`
-        ] || null;
+    if (pipelineJson === null) {
+        throw new UnexpectedError(`Template for formfactor "${formfactorName}" not found`);
+    }
 
     return pipelineJson;
 }
 
 /**
+ * TODO: !!!!!! Test
  * TODO: [🧠] Which is the best place for this function
  * TODO: !!!!!! `book string template notation
  */
