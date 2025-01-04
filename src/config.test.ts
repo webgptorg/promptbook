4aeaccf060f6ee34d36d35945b37fe2a485f0487
Pavol Hejny <me@pavolhejny.com>
2025-01-04 15:56:55 +0100

    [🔑] Permanently **disable** cost-prevention

--- src/config.test.ts
+++ src/config.test.ts
@@ -1,11 +1,12 @@
 import { describe, expect, it } from '@jest/globals';
-import { IS_COST_PREVENTED } from './config';
 import { IS_PIPELINE_LOGIC_VALIDATED } from './config';
 
 describe('configuration', () => {
+    /*/
     it('prevents accidental costs', () => {
         expect(IS_COST_PREVENTED).toBe(true);
     });
+    /**/
 
     it('checks examples logic', () => {
         expect(IS_PIPELINE_LOGIC_VALIDATED).toBe(true);
