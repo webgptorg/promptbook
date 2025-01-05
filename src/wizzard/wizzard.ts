9593ed6eafc19d8992e69a61120977a47a35b395
Pavol Hejny <me@pavolhejny.com>
2025-01-05 16:21:58 +0100

    Working on wizzard

--- src/wizzard/wizzard.ts
+++ src/wizzard/wizzard.ts
@@ -1,30 +1,46 @@
+import spaceTrim from 'spacetrim';
 import { Promisable } from 'type-fest';
 import {
     $provideExecutablesForNode,
     $provideFilesystemForNode,
     $provideScrapersForNode,
 } from '../_packages/node.index';
-import { $isRunningInNode } from '../_packages/utils.index';
+import { PipelineJson } from '../_packages/types.index';
+import { $isRunningInNode, isValidFilePath, isValidUrl } from '../_packages/utils.index';
 import { createCollectionFromDirectory } from '../collection/constructors/createCollectionFromDirectory';
 import { EnvironmentMismatchError } from '../errors/EnvironmentMismatchError';
+import { NotFoundError } from '../errors/NotFoundError';
+import { NotYetImplementedError } from '../errors/NotYetImplementedError';
 import { assertsExecutionSuccessful } from '../execution/assertsExecutionSuccessful';
 import { createPipelineExecutor } from '../execution/createPipelineExecutor/00-createPipelineExecutor';
 import { ExecutionTools } from '../execution/ExecutionTools';
 import type { PipelineExecutorResult } from '../execution/PipelineExecutorResult';
 import { $provideLlmToolsForWizzardOrCli } from '../llm-providers/_common/register/$provideLlmToolsForWizzardOrCli';
+import { PipelineString } from '../pipeline/PipelineString';
 import type { TaskProgress } from '../types/TaskProgress';
-import type { InputParameters, string_pipeline_url } from '../types/typeAliases';
+import type { InputParameters, string_filename, string_pipeline_url } from '../types/typeAliases';
+import { just } from '../utils/organization/just';
 
 /**
  * @@@
  *
+ * @singleton
  * @public exported from `@promptbook/wizzard`
  */
-export const wizzard = {
+export const wizzard = new Wizzard();
+
+/**
+ * Look at `wizzard` for more details
+ *
+ * @private just for single instance
+ */
+class Wizzard {
+    private constructor() {}
+
     /**
      * @@@!!!!!!
      */
-    async execute(
+    public async execute(
         book: string_pipeline_url,
         inputParameters: InputParameters,
         onProgress?: (taskProgress: TaskProgress) => Promisable<void>,
@@ -33,7 +49,37 @@ export const wizzard = {
             throw new EnvironmentMismatchError('Wizzard works only in Node.js environment');
         }
 
-        // ▶ Prepare tools
+        // ▶ Get the tools
+        const tools = await this.getExecutionTools();
+
+        // ▶ Get the Pipeline
+        const pipeline = await this.getPipeline(book);
+
+        // ▶ Create executor - the function that will execute the Pipeline
+        const pipelineExecutor = createPipelineExecutor({ pipeline, tools });
+
+        // 🚀▶ Execute the Pipeline
+        const result = await pipelineExecutor(inputParameters, onProgress);
+
+        // ▶ Fail if the execution was not successful
+        assertsExecutionSuccessful(result);
+
+        // ▶ Return the result
+        return result;
+    }
+
+    private executionTools: ExecutionTools | null = null;
+
+    /**
+     * @@@!!!
+     *
+     * @param pipelineSource
+     */
+    public async getExecutionTools(): Promise<ExecutionTools> {
+        if (this.executionTools !== null) {
+            return this.executionTools;
+        }
+
         // TODO: DRY [◽]
         const prepareAndScrapeOptions = {
             isVerbose: false,
@@ -52,28 +98,85 @@ export const wizzard = {
             ],
         } satisfies ExecutionTools;
 
-        // ▶ Create whole pipeline collection
-        const collection = await createCollectionFromDirectory('./books', tools);
-        // <- TODO: !!!!!! Search recursively in the directory + allow to pass relative path or book string template or just string
+        this.executionTools = tools;
 
-        // ▶ Get single Pipeline
-        const pipeline = await collection.getPipelineByUrl(book);
+        return tools;
+    }
 
-        // ▶ Create executor - the function that will execute the Pipeline
-        const pipelineExecutor = createPipelineExecutor({ pipeline, tools });
+    /**
+     * @@@!!!
+     *
+     * Strategies:
+     * 1) @@@!!!
+     * 2) @@@!!!
+     *
+     * @param pipelineSource
+     */
+    public async getPipeline(
+        pipelineSource: string_filename | string_pipeline_url | PipelineString,
+    ): Promise<PipelineJson> {
+        // ▶ Get the tools
+        const tools = await this.getExecutionTools();
 
-        // 🚀▶ Execute the Pipeline
-        const result = await pipelineExecutor(inputParameters, onProgress);
+        // Strategy 1️⃣: If the pipelineSource is a filename - try to load it from the file
+        if (isValidFilePath(pipelineSource)) {
+            // TODO: !!!!!! Implement
+        } /* not else */
 
-        // ▶ Fail if the execution was not successful
-        assertsExecutionSuccessful(result);
+        // Strategy 2️⃣: If the pipelineSource is a URL - try to find the pipeline on disk in `DEFAULT_BOOKS_DIRNAME` (= `./books`) directory recursively up to the root
+        if (isValidUrl(pipelineSource)) {
+            // ▶ Create whole pipeline collection
+            const collection = await createCollectionFromDirectory('./books', tools);
+            // <- TODO: !!!!!! Search recursively in the directory
 
-        // ▶ Return the result
-        return result;
-    },
-};
+            // ▶ Get single Pipeline
+            const pipeline = await (async () => {
+                try {
+                    return await collection.getPipelineByUrl(pipelineSource);
+                } catch (error) {
+                    if (!(error instanceof NotFoundError)) {
+                        throw error;
+                    }
+
+                    // Note: If the pipeline was not found in the collection, try next strategy
+                    return null;
+                }
+            })();
+
+            if (pipeline !== null) {
+                return pipeline;
+            }
+        } /* not else */
+
+        // Strategy 3️⃣: If the pipelineSource is a URL - try to fetch it from the internet
+        if (isValidUrl(pipelineSource)) {
+            throw new NotYetImplementedError(
+                'Strategy 3️⃣: If the pipelineSource is a URL - try to fetch it from the internet',
+            );
+        } /* not else */
+
+        // Strategy 4️⃣: If the pipelineSource is a PipelineString - try to parse it
+        if (just(true) /* <- TODO: Implement, use and export `isValidPipelineString` */) {
+            throw new NotYetImplementedError(
+                'Strategy 4️⃣: If the pipelineSource is a PipelineString - try to parse it',
+            );
+        } /* not else */
+
+        throw new NotFoundError(
+            spaceTrim(
+                (block) => `
+                    No pipeline found for "${pipelineSource}"
+
+                    Pipelines can be loaded from:
+                    1) @@@!!!
+                `,
+            ),
+        );
+    }
+}
 
 /**
+ * TODO: !!!!!! Mark in jsdoc as non-pure
  * TODO: !!!!!! Add to readmes - one markdown here imported in all packages
  * Note: [🟢] Code in this file should never be never released in packages that could be imported into browser environment
  */
