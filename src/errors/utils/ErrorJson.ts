26c5842b26d92ea5b4ed29ff0eef1a00406d1b8a
Pavol Hejny <me@pavolhejny.com>
2025-01-05 15:14:48 +0100

    TODOs

--- src/errors/utils/ErrorJson.ts
+++ src/errors/utils/ErrorJson.ts
@@ -1,4 +1,4 @@
-import type { ALL_ERRORS } from '../0-index'; // <- TODO: !!!!!! [🤛] This should be this automatically repared to type import, but its not
+import type { ALL_ERRORS } from '../0-index'; // <- TODO: [🤛] This should be this automatically repared to type import, but its not
 
 /**
  * Represents a serialized error or custom Promptbook error
