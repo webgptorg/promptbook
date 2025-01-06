35b9a1e95777de46f64e9aac3bb445d4ea39408a
Pavol Hejny <me@pavolhejny.com>
2025-01-06 21:20:25 +0100

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/templates.index.ts
+++ src/_packages/templates.index.ts
@@ -1,11 +1,13 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/templates`
 
-import { getBookTemplates } from '../other/templates/getBookTemplates';
 import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+import { getBookTemplates } from '../other/templates/getBookTemplates';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/templates`
-export { getBookTemplates as getBookTemplate };
+export { getBookTemplates };
