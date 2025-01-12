57deb671bac3ff785ff673b897535fc99df37ebd
Pavol Hejny <me@pavolhejny.com>
2025-01-12 20:31:12 +0100

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/remote-client.index.ts
+++ src/_packages/remote-client.index.ts
@@ -1,14 +1,21 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/remote-client`
 
+import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+import { compilePipelineOnRemoteServer } from '../conversion/compilePipelineOnRemoteServer';
 import { RemoteLlmExecutionTools } from '../llm-providers/remote/RemoteLlmExecutionTools';
+import { preparePipelineOnRemoteServer } from '../prepare/preparePipelineOnRemoteServer';
 import type { RemoteLlmExecutionToolsOptions } from '../remote-server/interfaces/RemoteLlmExecutionToolsOptions';
 import type { RemoteServerOptions } from '../remote-server/interfaces/RemoteServerOptions';
-import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/remote-client`
+export { compilePipelineOnRemoteServer };
 export { RemoteLlmExecutionTools };
-export type { RemoteLlmExecutionToolsOptions, RemoteServerOptions };
+export { preparePipelineOnRemoteServer };
+export type { RemoteLlmExecutionToolsOptions };
+export type { RemoteServerOptions };
--- src/_packages/remote-server.index.ts
+++ src/_packages/remote-server.index.ts
@@ -1,11 +1,15 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/remote-server`
 
-import { startRemoteServer } from '../remote-server/startRemoteServer';
 import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+import type { RemoteServerOptions } from '../remote-server/interfaces/RemoteServerOptions';
+import { startRemoteServer } from '../remote-server/startRemoteServer';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/remote-server`
+export type { RemoteServerOptions };
 export { startRemoteServer };
