d2e646410f2c5b623f3470b8b6091685536f54b5
Pavol Hejny <me@pavolhejny.com>
2025-01-14 05:13:36 +0100

    📦 Generating packages
    🔼 This commit was automatically generated by map scripts

--- src/_packages/remote-client.index.ts
+++ src/_packages/remote-client.index.ts
@@ -1,16 +1,27 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/remote-client`
 
+import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
 import { compilePipelineOnRemoteServer } from '../conversion/compilePipelineOnRemoteServer';
 import { RemoteLlmExecutionTools } from '../llm-providers/remote/RemoteLlmExecutionTools';
 import { preparePipelineOnRemoteServer } from '../prepare/preparePipelineOnRemoteServer';
+import type { PromptbookServer_Identification } from '../remote-server/socket-types/_subtypes/PromptbookServer_Identification';
+import type { PromptbookServer_ApplicationIdentification } from '../remote-server/socket-types/_subtypes/PromptbookServer_Identification';
+import type { PromptbookServer_AnonymousIdentification } from '../remote-server/socket-types/_subtypes/PromptbookServer_Identification';
 import type { RemoteClientOptions } from '../remote-server/types/RemoteClientOptions';
 import type { RemoteServerOptions } from '../remote-server/types/RemoteServerOptions';
-import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/remote-client`
-export { compilePipelineOnRemoteServer, preparePipelineOnRemoteServer, RemoteLlmExecutionTools };
-export type { RemoteClientOptions as RemoteLlmExecutionToolsOptions, RemoteServerOptions };
+export { compilePipelineOnRemoteServer };
+export { RemoteLlmExecutionTools };
+export { preparePipelineOnRemoteServer };
+export type { PromptbookServer_Identification };
+export type { PromptbookServer_ApplicationIdentification };
+export type { PromptbookServer_AnonymousIdentification };
+export type { RemoteClientOptions };
+export type { RemoteServerOptions };
