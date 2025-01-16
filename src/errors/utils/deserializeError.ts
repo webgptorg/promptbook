8577cc35d74b164db832e73b06c1769124302db0
Pavol Hejny <me@pavolhejny.com>
2025-01-16 11:40:59 +0100

    ✨ Enhance error deserialization with improved message formatting and stack trace handling

--- src/errors/utils/deserializeError.ts
+++ src/errors/utils/deserializeError.ts
@@ -1,3 +1,4 @@
+import spaceTrim from 'spacetrim';
 import { ALL_ERRORS } from '../0-index';
 import type { ErrorJson } from './ErrorJson';
 
@@ -6,13 +7,24 @@ import type { ErrorJson } from './ErrorJson';
  *
  * @public exported from `@promptbook/utils`
  */
-
 export function deserializeError(error: ErrorJson): Error {
-    const ErrorClass = ALL_ERRORS[error.name as keyof typeof ALL_ERRORS];
+    const { name, stack } = error;
+    let { message } = error;
+    let ErrorClass = ALL_ERRORS[error.name as keyof typeof ALL_ERRORS];
 
     if (ErrorClass === undefined) {
-        return new Error(`${error.name}: ${error.message}`);
+        ErrorClass = Error;
+        message = `${name}: ${message}`;
     }
 
-    return new ErrorClass(error.message);
+    return new ErrorClass(
+        spaceTrim(
+            (block) => `
+                ${block(message)}
+
+                Original stack trace:
+                ${block(stack || '')}
+            `,
+        ),
+    );
 }
