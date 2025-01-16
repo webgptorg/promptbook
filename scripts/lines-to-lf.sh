b8146cec09acd4feea433fa48ba672cf86f0ed25
Pavol Hejny <me@pavolhejny.com>
2025-01-16 15:57:30 +0100

    🏭🩹 Fix end of lines to LF

---
+++ scripts/lines-to-lf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+folders=(
+    ".vscode/**"
+    ".github/**"
+    "examples/**"
+    "book/**"
+    "other/**"
+    "personas/**"
+    "scripts/**"
+    "books/**"
+    "documents/**"
+    "src/**"
+    "CHANGELOG.md"
+    "package-lock.json"
+    "README.md"
+    "TODO.md"
+    "rollup.config.js"
+    "tsconfig.json"
+    "DICTIONARY.md"
+    "jest.config.js"
+    "jest.setup.js"
+    "package.json"
+    "PROGRAMMING.md"
+)
+
+for fileOrFolder in "${folders[@]}"; do
+    # Note: Recursively find all files, excluding node_modules
+    find $fileOrFolder -type f -not -path "*/node_modules/*" -exec dos2unix {} \;
+done
