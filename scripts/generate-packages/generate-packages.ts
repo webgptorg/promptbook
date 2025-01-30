be5f2699528dff89d2e2ad5d4c1ae1e7b7955f25
Pavol Hejny <me@pavolhejny.com>
2025-01-30 16:53:33 +0100

    Try `UNLICENSED`

--- scripts/generate-packages/generate-packages.ts
+++ scripts/generate-packages/generate-packages.ts
@@ -277,7 +277,8 @@ async function generatePackages({ isCommited, isBundlerSkipped }: { isCommited: 
         if (packageFullname === '@promptbook/utils' || packageFullname === '@promptbook/markdown-utils') {
             packageJson.license = 'CC-BY-4.0';
         } else {
-            packageJson.license = 'FSL-1.1-Apache-2.0';
+            packageJson.license = 'UNLICENSED';
+            // <- TODO: !!!!!
             // <- Note: [🥙] Since FSL is not yet in the SPDX license list, this is the most accurate way to represent it while maintaining compatibility with package managers
         }
 
