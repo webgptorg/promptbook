eae2a5765af37e24db48dbec57fb6accc4cc8700
Pavol Hejny <me@pavolhejny.com>
2025-01-31 13:39:58 +0100

    TODOs

--- src/prepare/preparePipelineOnRemoteServer.ts
+++ src/prepare/preparePipelineOnRemoteServer.ts
@@ -49,5 +49,5 @@ export async function preparePipelineOnRemoteServer<TCustomOptions = undefined>(
 }
 
 /**
- * TODO: !!!! Do not return Promise<PipelineJson> But PreparePipelineTask
+ * TODO: [🐚] Do not return Promise<PipelineJson> But PreparePipelineTask
  */
