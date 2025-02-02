4dcdf62547f1c205750e8c993a5377d2215b0c3f
Pavol Hejny <me@pavolhejny.com>
2025-02-02 11:53:07 +0100

    `npx ptbk` should work without any previous install

---
+++ packages/ptbk/bin/promptbook-cli-proxy.js
@@ -0,0 +1,6 @@
+// TODO: !!!!!! Remove or proxy
+
+/**
+ * If kept:
+ * TODO: !!! During the build check that this file exists
+ */
--- scripts/generate-packages/generate-packages.ts
+++ scripts/generate-packages/generate-packages.ts
@@ -493,17 +493,17 @@ async function generatePackages({ isCommited, isBundlerSkipped }: { isCommited: 
             // <- Note: [🧃] Using only `dependencies` (not `devDependencies`)
         }
 
-        if (
-            packageFullname === '@promptbook/cli' /* || packageFullname === '@promptbook/ptbk'
-                                                      <- TODO: !!! Is `npx ptbk` without previous install working without this commented condition?
-            */
-        ) {
+        if (packageFullname === '@promptbook/cli') {
             packageJson.bin = {
                 promptbook: 'bin/promptbook-cli.js',
                 ptbk: 'bin/promptbook-cli.js',
                 book: 'bin/promptbook-cli.js',
                 bk: 'bin/promptbook-cli.js',
                 // <- TODO: [🧠] Pick one of and remove rest
+            };
+        } else if (packageFullname === '@promptbook/ptbk') {
+            packageJson.bin = {
+                ptbk: '../@promptbook/cli/bin/promptbook-cli.js',
             };
         }
 
