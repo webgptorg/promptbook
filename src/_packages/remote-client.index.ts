5321bb9f22424ebe17b3417b2324604ed1cdf9d1
Pavol Hejny <me@pavolhejny.com>
2025-04-01 13:56:57 +0200

    📦 Generating packages `0.89.0-8`
    🔼 This commit was automatically generated by map scripts

--- src/_packages/remote-client.index.ts
+++ src/_packages/remote-client.index.ts
@@ -1,25 +1,25 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/remote-client`
 
+import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
 import { compilePipelineOnRemoteServer } from '../conversion/compilePipelineOnRemoteServer';
 import { RemoteLlmExecutionTools } from '../llm-providers/remote/RemoteLlmExecutionTools';
 import { preparePipelineOnRemoteServer } from '../prepare/preparePipelineOnRemoteServer';
-import type {
-    AnonymousModeIdentification,
-    ApplicationModeIdentification,
-    Identification,
-} from '../remote-server/socket-types/_subtypes/Identification';
+import type { Identification } from '../remote-server/socket-types/_subtypes/Identification';
+import type { ApplicationModeIdentification } from '../remote-server/socket-types/_subtypes/Identification';
+import type { AnonymousModeIdentification } from '../remote-server/socket-types/_subtypes/Identification';
 import type { RemoteClientOptions } from '../remote-server/types/RemoteClientOptions';
-import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/remote-client`
-export { compilePipelineOnRemoteServer, preparePipelineOnRemoteServer, RemoteLlmExecutionTools };
-export type {
-    AnonymousModeIdentification as PromptbookServer_AnonymousIdentification,
-    ApplicationModeIdentification as PromptbookServer_ApplicationIdentification,
-    Identification as PromptbookServer_Identification,
-    RemoteClientOptions,
-};
+export { compilePipelineOnRemoteServer };
+export { RemoteLlmExecutionTools };
+export { preparePipelineOnRemoteServer };
+export type { Identification };
+export type { ApplicationModeIdentification };
+export type { AnonymousModeIdentification };
+export type { RemoteClientOptions };
--- src/_packages/remote-server.index.ts
+++ src/_packages/remote-server.index.ts
@@ -1,23 +1,21 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/remote-server`
 
-import type {
-    AnonymousModeIdentification,
-    ApplicationModeIdentification,
-    Identification,
-} from '../remote-server/socket-types/_subtypes/Identification';
+import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+import type { Identification } from '../remote-server/socket-types/_subtypes/Identification';
+import type { ApplicationModeIdentification } from '../remote-server/socket-types/_subtypes/Identification';
+import type { AnonymousModeIdentification } from '../remote-server/socket-types/_subtypes/Identification';
 import { startRemoteServer } from '../remote-server/startRemoteServer';
 import type { RemoteServerOptions } from '../remote-server/types/RemoteServerOptions';
-import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/remote-server`
+export type { Identification };
+export type { ApplicationModeIdentification };
+export type { AnonymousModeIdentification };
 export { startRemoteServer };
-export type {
-    AnonymousModeIdentification as PromptbookServer_AnonymousIdentification,
-    ApplicationModeIdentification as PromptbookServer_ApplicationIdentification,
-    Identification as PromptbookServer_Identification,
-    RemoteServerOptions,
-};
+export type { RemoteServerOptions };
