e1300f3d5fa576642c501a4d6deb8bab0c5dc73d
Pavol Hejny <me@pavolhejny.com>
2025-04-02 14:47:17 +0200

    Cleanup & install

--- src/cli/test/ptbk.ts
+++ src/cli/test/ptbk.ts
@@ -4,7 +4,7 @@
  * Note: [🔺] Purpose of this file is to test and use the current CLI in development environment
  */
 
-import '../../_packages/cli.index.ts'; // <- Note: Register all the LLM providers, scrapers, etc. by importing this file
+import '../../_packages/cli.index'; // <- Note: Register all the LLM providers, scrapers, etc. by importing this file
 import { promptbookCli } from '../promptbookCli';
 
 promptbookCli();
