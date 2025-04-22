d91978173527a8df3d57e817b8f9841bdeee1305
Pavol Hejny <me@pavolhejny.com>
2025-04-23 00:49:41 +0200

    📦 Generating packages `0.92.0-3`
    🔼 This commit was automatically generated by map scripts

--- src/_packages/deepseek.index.ts
+++ src/_packages/deepseek.index.ts
@@ -3,6 +3,7 @@
 
 import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
 import { createDeepseekExecutionTools } from '../llm-providers/deepseek/createDeepseekExecutionTools';
+import { DEEPSEEK_MODELS } from '../llm-providers/deepseek/deepseek-models';
 import type { DeepseekExecutionToolsOptions } from '../llm-providers/deepseek/DeepseekExecutionToolsOptions';
 import { _DeepseekRegistration } from '../llm-providers/deepseek/register-constructor';
 
@@ -13,5 +14,6 @@ export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
 // Note: Entities of the `@promptbook/deepseek`
 export { createDeepseekExecutionTools };
+export { DEEPSEEK_MODELS };
 export type { DeepseekExecutionToolsOptions };
 export { _DeepseekRegistration };
