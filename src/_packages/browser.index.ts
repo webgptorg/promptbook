3f9e930562fb88ec01e23654a100b107ff23c614
Pavol Hejny <me@pavolhejny.com>
2025-04-26 15:07:05 +0200

    📦 Generating packages `0.92.0-11`
    🔼 This commit was automatically generated by map scripts

--- src/_packages/browser.index.ts
+++ src/_packages/browser.index.ts
@@ -4,6 +4,7 @@
 import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
 import { SimplePromptInterfaceTools } from '../dialogs/simple-prompt/SimplePromptInterfaceTools';
 import { $provideScrapersForBrowser } from '../scrapers/_common/register/$provideScrapersForBrowser';
+import { getIndexedDbStorage } from '../storage/local-storage/getIndexedDbStorage';
 import { getLocalStorage } from '../storage/local-storage/getLocalStorage';
 import { getSessionStorage } from '../storage/local-storage/getSessionStorage';
 
@@ -15,5 +16,6 @@ export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 // Note: Entities of the `@promptbook/browser`
 export { SimplePromptInterfaceTools };
 export { $provideScrapersForBrowser };
+export { getIndexedDbStorage };
 export { getLocalStorage };
 export { getSessionStorage };
