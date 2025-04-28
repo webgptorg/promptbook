d9770aa3bc28db7f4787cc10f41e01aeaa56cd47
Pavol Hejny <me@pavolhejny.com>
2025-04-29 01:34:36 +0200

    `DEFAULT_RPM` is public

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,7 @@
 import { REMOTE_SERVER_URLS } from '../servers';
 import type { CsvSettings } from './formats/csv/CsvSettings';
 import type { IntermediateFilesStrategy } from './types/IntermediateFilesStrategy';
-import type { string_app_id } from './types/typeAliases';
-import type { string_email } from './types/typeAliases';
-import type { string_name } from './types/typeAliases';
-import type { string_promptbook_server_url } from './types/typeAliases';
+import type { string_app_id, string_email, string_name, string_promptbook_server_url } from './types/typeAliases';
 import { just } from './utils/organization/just';
 
 /**
@@ -340,7 +337,9 @@ export const DEFAULT_GET_PIPELINE_COLLECTION_FUNCTION_NAME = `getPipelineCollect
 /**
  * Default rate limits (requests per minute)
  *
- * Note: Adjust based on OpenAI tier
+ * Note: Adjust based on the provider tier you are have
+ *
+ * @public exported from `@promptbook/core`
  */
 export const DEFAULT_RPM = 60;
 
