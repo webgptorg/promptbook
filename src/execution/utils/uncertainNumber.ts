b1ce8e34fed03f77b1974d8fe75a85836df0ca93
Pavol Hejny <me@pavolhejny.com>
2025-05-06 13:51:37 +0200

    Model is not in the list of known models, maybe just a different version of the same model + Better logging for OpenAI usage

--- src/execution/utils/uncertainNumber.ts
+++ src/execution/utils/uncertainNumber.ts
@@ -4,13 +4,21 @@ import { UNCERTAIN_ZERO_VALUE } from './usage-constants';
 /**
  * Make UncertainNumber
  *
- * @param value
+ * @param value value of the uncertain number, if `NaN` or `undefined`, it will be set to 0 and `isUncertain=true`
+ * @param isUncertain if `true`, the value is uncertain, otherwise depends on the value
  *
  * @private utility for initializating UncertainNumber
  */
-export function uncertainNumber(value?: number | typeof NaN | undefined | null): UncertainNumber {
+export function uncertainNumber(
+    value?: number | typeof NaN | undefined | null,
+    isUncertain?: boolean,
+): UncertainNumber {
     if (value === null || value === undefined || Number.isNaN(value)) {
         return UNCERTAIN_ZERO_VALUE;
+    }
+
+    if (isUncertain === true) {
+        return { value, isUncertain };
     }
 
     return { value };
