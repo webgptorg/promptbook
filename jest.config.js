fd295ecb61a2c05aa4b19ac620ef1bad9354c6cd
Pavol Hejny <me@pavolhejny.com>
2025-07-09 18:26:52 +0200

    Revert "Fixing jest problem *(going to revert)*"
    This reverts commit e577749f36b654ee7c38d0a72e497bd39ec93734.

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-        '^.+\\.(js|jsx|mjs)$': 'ts-jest',
+        '(jsx?|tsx?)$': 'ts-jest',
     },
     testRegex: '(test)\\.(jsx?|tsx?)$',
     setupFilesAfterEnv: ['./jest.setup.js'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     coverageDirectory: './coverage/',
     collectCoverage: true,
     testTimeout: 5 /* minutes */ * 60 * 1000,
-    transformIgnorePatterns: [
-        'node_modules/(?!(@azure|@promptbook|@ai-sdk|@anthropic-ai)/)',
-    ],
-    extensionsToTreatAsEsm: ['.ts', '.tsx', '.mjs'],
-    globals: {
-        'ts-jest': {
-            useESM: true,
-            tsconfig: {
-                module: 'esnext',
-                target: 'es2020',
-                moduleResolution: 'node',
-                allowSyntheticDefaultImports: true,
-                esModuleInterop: true,
-            },
-        },
-    },
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
 };
