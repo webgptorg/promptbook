323ff64135b3635b40e36369c575dcc9b6d4e14d
Pavol Hejny <me@pavolhejny.com>
2025-08-07 15:27:30 +0200

    Bring Book 2.0 from Promptbook Studio - Fixing the types

--- src/book-2.0/commitments/_misc/parseAgentSourceWithCommitments.ts
+++ src/book-2.0/commitments/_misc/parseAgentSourceWithCommitments.ts
@@ -1,4 +1,4 @@
-import { spaceTrim } from '@promptbook/utils';
+import { spaceTrim } from 'spacetrim';
 import type { string_agent_name, string_url_image } from '../../../types/typeAliases';
 import type { AgentSourceBasicInformation } from '../../agent-source/parseAgentSource';
 import type { string_agent_source } from '../../agent-source/string_agent_source';
