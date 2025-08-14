af336d94b0be03efd3f4d80def5077419a6e47b4
Pavol Hejny <me@pavolhejny.com>
2025-08-14 13:40:36 +0200

    [✨📶] Make some testing playground for `@promptbook/components`

--- scripts/components-playground-server.ts
+++ scripts/components-playground-server.ts
@@ -220,6 +220,7 @@ async function main() {
                     <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
                     <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
                     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+                    <script src="https://cdn.tailwindcss.com"></script>
                     <style>
                         * {
                             margin: 0;
@@ -582,6 +583,7 @@ async function main() {
                     <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
                     <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
                     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+                    <script src="https://cdn.tailwindcss.com"></script>
                     <style>
                         body {
                             margin: 0;
