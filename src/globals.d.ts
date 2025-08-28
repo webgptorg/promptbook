9e6bbe5c2fbcb8bc980d6b9649916f23f7a9f77b
Pavol Hejny <me@pavolhejny.com>
2025-08-28 15:40:32 +0200

    `BookEditor` - Move CSS module type definitions -> `globals.d.ts`

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -3,4 +3,7 @@ declare module '*.book';
 declare module '*.book.md';
 declare module '*.bookc';
 
+// Note: For the components
+declare module '*.module.css';
+
 // Note: When some module does not have types available, it is imported using `require`
