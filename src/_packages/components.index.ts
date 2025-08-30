6196662ae1ed661ab78d52e9ca9954db64ea41fa
Pavol Hejny <me@pavolhejny.com>
2025-08-30 11:33:58 +0200

    Publishing

--- src/_packages/components.index.ts
+++ src/_packages/components.index.ts
@@ -6,9 +6,14 @@ import type { AvatarChipProps } from '../book-components/AvatarProfile/AvatarChi
 import { AvatarChip } from '../book-components/AvatarProfile/AvatarChip/AvatarChip';
 import type { AvatarChipFromSourceProps } from '../book-components/AvatarProfile/AvatarChip/AvatarChipFromSource';
 import { AvatarChipFromSource } from '../book-components/AvatarProfile/AvatarChip/AvatarChipFromSource';
+import type { AvatarProfileProps } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfile';
+import { AvatarProfile } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfile';
+import type { AvatarProfileFromSourceProps } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfileFromSource';
+import { AvatarProfileFromSource } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfileFromSource';
 import type { BookEditorProps } from '../book-components/BookEditor/BookEditor';
 import { BookEditor } from '../book-components/BookEditor/BookEditor';
 import { DEFAULT_BOOK_FONT_CLASS } from '../book-components/BookEditor/config';
+import { Chat } from '../book-components/Chat/Chat/Chat';
 
 
 // Note: Exporting version from each package
@@ -20,6 +25,11 @@ export type { AvatarChipProps };
 export { AvatarChip };
 export type { AvatarChipFromSourceProps };
 export { AvatarChipFromSource };
+export type { AvatarProfileProps };
+export { AvatarProfile };
+export type { AvatarProfileFromSourceProps };
+export { AvatarProfileFromSource };
 export type { BookEditorProps };
 export { BookEditor };
 export { DEFAULT_BOOK_FONT_CLASS };
+export { Chat };
