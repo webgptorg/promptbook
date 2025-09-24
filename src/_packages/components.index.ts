078c246bbe51bc8b04721e8bf7a122448a355f88
Pavol Hejny <me@pavolhejny.com>
2025-09-24 08:33:16 +0200

    Autogenerated

--- src/_packages/components.index.ts
+++ src/_packages/components.index.ts
@@ -3,6 +3,7 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/components`
 
+import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
 import type { AvatarChipProps } from '../book-components/AvatarProfile/AvatarChip/AvatarChip';
 import { AvatarChip } from '../book-components/AvatarProfile/AvatarChip/AvatarChip';
 import type { AvatarChipFromSourceProps } from '../book-components/AvatarProfile/AvatarChip/AvatarChipFromSource';
@@ -11,7 +12,8 @@ import type { AvatarProfileProps } from '../book-components/AvatarProfile/Avatar
 import { AvatarProfile } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfile';
 import type { AvatarProfileFromSourceProps } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfileFromSource';
 import { AvatarProfileFromSource } from '../book-components/AvatarProfile/AvatarProfile/AvatarProfileFromSource';
-import type { MockedChatDelayConfig, MockedChatProps } from '../book-components/AvatarProfile/AvatarProfile/MockedChat';
+import type { MockedChatDelayConfig } from '../book-components/AvatarProfile/AvatarProfile/MockedChat';
+import type { MockedChatProps } from '../book-components/AvatarProfile/AvatarProfile/MockedChat';
 import { MockedChat } from '../book-components/AvatarProfile/AvatarProfile/MockedChat';
 import type { BookEditorProps } from '../book-components/BookEditor/BookEditor';
 import { BookEditor } from '../book-components/BookEditor/BookEditor';
@@ -22,44 +24,42 @@ import { LlmChat } from '../book-components/Chat/LlmChat/LlmChat';
 import type { LlmChatProps } from '../book-components/Chat/LlmChat/LlmChatProps';
 import type { ChatMessage } from '../book-components/Chat/types/ChatMessage';
 import type { ChatParticipant } from '../book-components/Chat/types/ChatParticipant';
-import { isMarkdownContent, renderMarkdown } from '../book-components/Chat/utils/renderMarkdown';
+import { renderMarkdown } from '../book-components/Chat/utils/renderMarkdown';
+import { isMarkdownContent } from '../book-components/Chat/utils/renderMarkdown';
 import { ArrowIcon } from '../book-components/icons/ArrowIcon';
 import { ResetIcon } from '../book-components/icons/ResetIcon';
 import { SendIcon } from '../book-components/icons/SendIcon';
 import { TemplateIcon } from '../book-components/icons/TemplateIcon';
-import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/components`
-export {
-    ArrowIcon,
-    AvatarChip,
-    AvatarChipFromSource,
-    AvatarProfile,
-    AvatarProfileFromSource,
-    BookEditor,
-    Chat,
-    DEFAULT_BOOK_FONT_CLASS,
-    isMarkdownContent,
-    LlmChat,
-    MockedChat,
-    renderMarkdown,
-    ResetIcon,
-    SendIcon,
-    TemplateIcon,
-};
-export type {
-    AvatarChipFromSourceProps,
-    AvatarChipProps,
-    AvatarProfileFromSourceProps,
-    AvatarProfileProps,
-    BookEditorProps,
-    ChatMessage,
-    ChatParticipant,
-    ChatProps,
-    LlmChatProps,
-    MockedChatDelayConfig,
-    MockedChatProps,
-};
+export type { AvatarChipProps };
+export { AvatarChip };
+export type { AvatarChipFromSourceProps };
+export { AvatarChipFromSource };
+export type { AvatarProfileProps };
+export { AvatarProfile };
+export type { AvatarProfileFromSourceProps };
+export { AvatarProfileFromSource };
+export type { MockedChatDelayConfig };
+export type { MockedChatProps };
+export { MockedChat };
+export type { BookEditorProps };
+export { BookEditor };
+export { DEFAULT_BOOK_FONT_CLASS };
+export { Chat };
+export type { ChatProps };
+export { LlmChat };
+export type { LlmChatProps };
+export type { ChatMessage };
+export type { ChatParticipant };
+export { renderMarkdown };
+export { isMarkdownContent };
+export { ArrowIcon };
+export { ResetIcon };
+export { SendIcon };
+export { TemplateIcon };
