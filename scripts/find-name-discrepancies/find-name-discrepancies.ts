8165b0515f4384a551797e94cf9ac154066852df
Pavol Hejny <me@pavolhejny.com>
2025-10-08 14:17:52 +0200

    Better `find-name-discrepancies`

--- scripts/find-name-discrepancies/find-name-discrepancies.ts
+++ scripts/find-name-discrepancies/find-name-discrepancies.ts
@@ -37,47 +37,89 @@ async function findNameDiscrepancies() {
 
     const filesWithEntities = await findAllProjectFilesWithEntities();
 
-    const filesWithDiscrepancies = filesWithEntities.filter(
-        ({ filename, entities }) => !entities.some(({ name }) => filename.includes(name)),
-    );
-
+    const filenamesIndexFiles: /*WritableDeep<Awaited<ReturnType<typeof findAllProjectFilesWithEntities>>>*/ Array<string> =
+        [];
     const filenamesWithDiscrepanciesNotIgnored: /*WritableDeep<Awaited<ReturnType<typeof findAllProjectFilesWithEntities>>>*/ Array<string> =
         [];
 
-    for (const file of filesWithDiscrepancies) {
-        const { filename } = file;
+    for (const file of filesWithEntities) {
+        const { filename, entities } = file;
 
+        /**/
+        // Note: Exclude package indexes in `/src/_packages`
+        if (filename.includes('src/_packages')) {
+            continue;
+        }
+        /**/
+
+        /**/
         // Note: Exclude some files from the check:
         if (filename.endsWith('openapi-types.ts')) {
             continue;
         }
+        /**/
 
         const content = await readFile(filename, 'utf-8');
 
         const isIgnored = content.includes('[💞]');
 
-        if (!isIgnored) {
+        if (isIgnored) {
+            continue;
+        }
+
+        const isFileIndexFile = filename.endsWith('.index.ts');
+        const isFileDiscrepantName = !entities.some(({ name }) => filename.includes(name));
+
+        if (isFileDiscrepantName) {
             filenamesWithDiscrepanciesNotIgnored.push(filename);
+        }
+
+        if (isFileIndexFile) {
+            filenamesIndexFiles.push(filename);
         }
     }
 
     for (const filename of filenamesWithDiscrepanciesNotIgnored) {
-        console.info(colors.yellow(`${filename}`));
+        console.info(colors.yellow(`${filename} <- File name not matching any of exported names`));
     }
 
-    if (filenamesWithDiscrepanciesNotIgnored.length !== 0) {
-        console.error(
-            colors.red(
-                spaceTrim(`
-                    Found ${filenamesWithDiscrepanciesNotIgnored.length} files with name discrepancies
+    for (const filename of filenamesIndexFiles) {
+        console.info(colors.yellow(`${filename} <- Not allowed index file`));
+    }
 
-                    Review the files listed above:
-                    1) Rename the entity in the file according to the file name
-                    2) Rename file to match the entity name
-                    3) Add Note: [💞] Ignore a discrepancy between file name and entity name
-                `),
-            ),
-        );
+    if (filenamesWithDiscrepanciesNotIgnored.length !== 0 || filenamesIndexFiles.length !== 0) {
+        if (filenamesWithDiscrepanciesNotIgnored.length !== 0) {
+            console.error(
+                colors.red(
+                    spaceTrim(`
+                        Found ${filenamesWithDiscrepanciesNotIgnored.length} files with name discrepancies
+
+                        Review the files listed above:
+                        1) Rename the entity in the file according to the file name
+                        2) Rename file to match the entity name
+                        3) Add Note: [💞] Ignore a discrepancy between file name and entity name
+                    `),
+                ),
+            );
+        }
+
+        if (filenamesWithDiscrepanciesNotIgnored.length !== 0 && filenamesIndexFiles.length !== 0) {
+            console.error('\n');
+        }
+
+        if (filenamesIndexFiles.length !== 0) {
+            console.error(
+                colors.red(
+                    spaceTrim(`
+                        Found ${filenamesIndexFiles.length} index files
+
+                        Review the files listed above:
+                        1) Extract the entities into separate files, **use named exports**
+                        2) Add Note: [💞] Allow this index file
+                    `),
+                ),
+            );
+        }
         process.exit(1);
     } else {
         console.info(colors.green(`No name discrepancies found`));
