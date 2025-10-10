24dabaed15275ef5a8f71c27921664056215ad55
Pavol Hejny <me@pavolhejny.com>
2025-10-10 16:23:56 +0200

    Prettier removed from Promptbook due to package size

--- src/utils/markdown/prettifyMarkdown.ts
+++ src/utils/markdown/prettifyMarkdown.ts
@@ -6,44 +6,10 @@ import type { string_html } from '../../types/typeAliases';
  * @param content raw html code
  * @returns formatted html code
  * @private withing the package because of HUGE size of prettier dependency
+ * @deprecated Prettier removed from Promptbook due to package size
  */
 export function prettifyMarkdown<TContent extends string_html>(content: TContent): TContent {
-    // In browser/Next.js environments, just return the original content
-    // since prettier parsers are not available and would cause bundling issues
-    if (typeof window !== 'undefined') {
-        return content;
-    }
-
-    try {
-        // Use dynamic require to avoid static imports that cause bundling issues
-        // This will only work in Node.js environments
-        const prettierStandalone = eval('require')('prettier/standalone');
-        const parserMarkdown = eval('require')('prettier/parser-markdown');
-        const parserHtml = eval('require')('prettier/parser-html');
-
-        return prettierStandalone.format(content, {
-            parser: 'markdown',
-            plugins: [parserMarkdown, parserHtml],
-
-            // TODO: DRY - make some import or auto-copy of .prettierrc
-            endOfLine: 'lf',
-            tabWidth: 4,
-            singleQuote: true,
-            trailingComma: 'all',
-            arrowParens: 'always',
-            printWidth: 120,
-            htmlWhitespaceSensitivity: 'ignore',
-            jsxBracketSameLine: false,
-            bracketSpacing: true,
-        }) as TContent;
-    } catch (error) {
-        // TODO: [🟥] Detect browser / node and make it colorful
-        console.error('There was an error with prettifying the markdown, using the original as the fallback', {
-            error,
-            html: content,
-        });
-        return content;
-    }
+    return (content + `\n\n<!-- Note: Prettier removed from Promptbook -->`) as TContent;
 }
 
 /**
@@ -52,37 +18,8 @@ export function prettifyMarkdown<TContent extends string_html>(content: TContent
  * @param content raw html code
  * @returns formatted html code
  * @private withing the package because of HUGE size of prettier dependency
+ * @deprecated Prettier removed from Promptbook due to package size
  */
 export async function prettifyMarkdownAsync<TContent extends string_html>(content: TContent): Promise<TContent> {
-    try {
-        // Use dynamic imports to avoid bundling issues in browser/Next.js environments
-        const [{ format }, parserMarkdown, parserHtml] = await Promise.all([
-            import('prettier/standalone'),
-            import('prettier/parser-markdown'),
-            import('prettier/parser-html'),
-        ]);
-
-        return format(content, {
-            parser: 'markdown',
-            plugins: [parserMarkdown.default, parserHtml.default],
-
-            // TODO: DRY - make some import or auto-copy of .prettierrc
-            endOfLine: 'lf',
-            tabWidth: 4,
-            singleQuote: true,
-            trailingComma: 'all',
-            arrowParens: 'always',
-            printWidth: 120,
-            htmlWhitespaceSensitivity: 'ignore',
-            jsxBracketSameLine: false,
-            bracketSpacing: true,
-        }) as TContent;
-    } catch (error) {
-        // TODO: [🟥] Detect browser / node and make it colorful
-        console.error('There was an error with prettifying the markdown, using the original as the fallback', {
-            error,
-            html: content,
-        });
-        return content;
-    }
+    return prettifyMarkdown(content);
 }
