a63cc4ba2ac57ad65d31dcdbef2e5102862c5048
Pavol Hejny <me@pavolhejny.com>
2025-11-01 11:02:50 +0100

    Revert "Update all dependencies by `npx npm-check-updates --upgrade --target patch`"
    This reverts commit 0c9750eb9061bd3b8fa4ab20ba33543e747b0c45.

--- src/transpilers/_common/register/$bookTranspilersRegister.ts
+++ src/transpilers/_common/register/$bookTranspilersRegister.ts
@@ -7,8 +7,6 @@ import type { BookTranspilerDefinition } from '../BookTranspilerDefinition';
  * Note: `$` is used to indicate that this interacts with the global scope
  * @singleton Only one instance of each register is created per build, but there can be more instances across different builds or environments.
  * @see https://github.com/webgptorg/promptbook/issues/249
- *
- * @public exported from `@promptbook/core`
  */
 export const $bookTranspilersRegister = new $Register<BookTranspilerDefinition>('book_transpilers');
 
