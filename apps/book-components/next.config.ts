68c29e1443e231663e929dfedaceec8385b3bc3a
Pavol Hejny <me@pavolhejny.com>
2025-11-02 12:02:20 +0100

    Fix build of `book-components`
    From https://vercel.com/pavol-hejns-projects/book-components/AcA5BDtHUex7Vaut8CcAnFrFtoEp :
    
    ```bash
    15:20:34.059
    15:20:34.059
    All dependencies                     578.54 MB
    15:20:34.059
    Serverless Function's page: index.js
    15:20:34.060
    Large Dependencies           Uncompressed size
    15:20:34.060
    apps/book-components/.next            568.7 MB
    15:20:34.060
    node_modules/next/dist                 4.14 MB
    15:20:34.060
    apps/book-components/public             3.1 MB
    15:20:34.060
    node_modules/react-dom/cjs             1.74 MB
    15:20:34.060
    15:20:34.060
    All dependencies                     578.53 MB
    15:20:34.060
    Serverless Function's page: utility/[id].js
    15:20:34.061
    Large Dependencies           Uncompressed size
    15:20:34.061
    apps/book-components/.next           568.71 MB
    15:20:34.061
    node_modules/next/dist                 4.14 MB
    15:20:34.061
    apps/book-components/public             3.1 MB
    15:20:34.061
    node_modules/react-dom/cjs             1.74 MB
    15:20:34.061
    15:20:34.061
    All dependencies                     578.54 MB
    15:20:34.062
    Max serverless function size was exceeded for 3 functions
    15:20:34.125
    Created all serverless functions in: 3.406s
    15:20:34.141
    Collected static files (public/, static/, .next/static): 11.28ms
    15:20:34.286
    Build Completed in /vercel/output [2m]
    15:20:34.479
    Deploying outputs...
    15:21:09.051
    Error: A Serverless Function has exceeded the unzipped maximum size of 250 MB. : https://vercel.link/serverless-function-size
    ```

--- apps/book-components/next.config.ts
+++ apps/book-components/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
     experimental: {
         externalDir: true,
     },
