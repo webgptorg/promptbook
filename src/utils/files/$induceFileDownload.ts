d4d7c39c2a56805bf054de19737db556fa1b63ae
Pavol Hejny <me@pavolhejny.com>
2025-11-14 04:09:43 +0100

    Fixing Vercel deployment issue

--- src/utils/files/$induceFileDownload.ts
+++ src/utils/files/$induceFileDownload.ts
@@ -1,8 +1,7 @@
 import { string_url } from '../../types/typeAliases';
-// import { $isRunningInBrowser } from '../environment/$isRunningInBrowser';
+import { $isRunningInBrowser } from '../environment/$isRunningInBrowser';
 import { $side_effect } from '../organization/$side_effect';
-import { TODO_USE } from '../organization/TODO_USE';
-// import { ObjectUrl } from './ObjectUrl';
+import { ObjectUrl } from './ObjectUrl';
 
 /**
  * Download a File in a browser
@@ -12,12 +11,6 @@ import { TODO_USE } from '../organization/TODO_USE';
  * @public exported from `@promptbook/browser`
  */
 export async function $induceFileDownload(fileOrBlobOrUrl: File | Blob | URL | string_url): Promise<$side_effect> {
-    TODO_USE(fileOrBlobOrUrl);
-
-    /*
-    TODO: !!!! Is this fixing Vercel deployment issue ??? !!!!
-
-
     if (!$isRunningInBrowser()) {
         throw new Error('Function `$induceFileDownload` is available ONLY in browser');
     }
@@ -25,10 +18,9 @@ export async function $induceFileDownload(fileOrBlobOrUrl: File | Blob | URL | s
     const objectUrl = ObjectUrl.fromBlobOrUrl(fileOrBlobOrUrl);
     const link = window.document.createElement('a');
     link.href = objectUrl.href;
-    link.download = (fileOrBlobOrUrl as File).name || 'untitled' /* <- TODO: Add proper extension according to url * /;
+    link.download = (fileOrBlobOrUrl as File).name || 'untitled' /* <- TODO: Add proper extension according to url */;
     link.click();
     await objectUrl.destroy();
-    */
 }
 
 /**
