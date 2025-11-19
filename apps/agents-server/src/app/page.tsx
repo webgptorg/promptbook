74cca16d700399f58d10852a8a1bdc0dd9e1d5e4
Pavol Hejny <me@pavolhejny.com>
2025-11-19 12:06:32 +0100

    `Agent` - Better homepage of Promptbook server

--- apps/agents-server/src/app/page.tsx
+++ apps/agents-server/src/app/page.tsx
@@ -41,54 +41,62 @@ export default async function HomePage() {
                     <Image src={logoImage} alt="Promptbook Logo" height={50} className="inline-block mr-4" />
                     Promptbook Agents Server
                 </h1>
-                <p className="text-xl text-gray-600 mb-12">agents server</p>
 
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                    <Link
-                        href={'#'}
-                        className="block p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 border border-gray-200 hover:border-blue-400"
-                    >
-                        <h2 className="text-2xl font-semibold text-gray-900 mb-2">
-                            Long running task {longRunningTask.taskId}
-                        </h2>
-                        <p className="text-gray-600">Tick: {longRunningTask.tick}</p>
-                        <p className="text-gray-600">
-                            Created At: {moment(longRunningTask.createdAt).calendar(undefined, calendarWithSeconds)}
-                        </p>
-                        <p className="text-gray-600">
-                            Updated At: {moment(longRunningTask.updatedAt).calendar(undefined, calendarWithSeconds)}
-                        </p>
-                    </Link>
-                </div>
+                <>
+                    <h2 className="text-3xl text-gray-900 mt-16 mb-4">Agents ({agents.length})</h2>
+                    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                        {agents.map((agent) => (
+                            <Link key={agent.agentName} href={`/agents/${agent.agentName}`}>
+                                <AvatarProfile
+                                    {...{ agent }}
+                                    className="block p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 border border-gray-200 hover:border-blue-400"
+                                />
+                            </Link>
+                        ))}
+                        <AddAgentButton />
+                    </div>
+                </>
 
-                <h2 className="text-3xl font-bold text-gray-900 mt-16 mb-4">Agents ({agents.length})</h2>
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                    {agents.map((agent) => (
-                        <Link key={agent.agentName} href={`/agents/${agent.agentName}`}>
-                            <AvatarProfile
-                                {...{ agent }}
-                                className="block p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 border border-gray-200 hover:border-blue-400"
-                            />
+                <>
+                    <h2 className="text-3xl text-gray-900 mt-16 mb-4">Models ({models.length})</h2>
+                    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                        {models.map(({ modelName, modelTitle, modelDescription }) => (
+                            <Link key={modelName} href={`#!!!`}>
+                                <div className="block p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 border border-gray-200 hover:border-blue-400">
+                                    <h2 className="text-2xl font-semibold text-gray-900 mb-2">{modelTitle}</h2>
+                                    <code>{modelName}</code>
+                                    <p className="text-gray-600">{modelDescription}</p>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </>
+
+                <>
+                    <h2 className="text-3xl text-gray-900 mt-16 mb-4">About</h2>
+                    <AboutPromptbookInformation />
+                </>
+
+                <>
+                    <h2 className="text-3xl text-gray-900 mt-16 mb-4">Technical Information</h2>
+                    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                        <Link
+                            href={'#'}
+                            className="block p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 border border-gray-200 hover:border-blue-400"
+                        >
+                            <h2 className="text-2xl font-semibold text-gray-900 mb-2">
+                                Long running task {longRunningTask.taskId}
+                            </h2>
+                            <p className="text-gray-600">Tick: {longRunningTask.tick}</p>
+                            <p className="text-gray-600">
+                                Created At: {moment(longRunningTask.createdAt).calendar(undefined, calendarWithSeconds)}
+                            </p>
+                            <p className="text-gray-600">
+                                Updated At: {moment(longRunningTask.updatedAt).calendar(undefined, calendarWithSeconds)}
+                            </p>
                         </Link>
-                    ))}
-                    <AddAgentButton />
-                </div>
-
-                <h2 className="text-3xl font-bold text-gray-900 mt-16 mb-4">Models ({models.length})</h2>
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                    {models.map(({ modelName, modelTitle, modelDescription }) => (
-                        <Link key={modelName} href={`#!!!`}>
-                            <div className="block p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 border border-gray-200 hover:border-blue-400">
-                                <h2 className="text-2xl font-semibold text-gray-900 mb-2">{modelTitle}</h2>
-                                <code>{modelName}</code>
-                                <p className="text-gray-600">{modelDescription}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-
-                <h2 className="text-3xl font-bold text-gray-900 mt-16 mb-4">About</h2>
-                <AboutPromptbookInformation />
+                    </div>
+                </>
             </div>
         </div>
     );
