91b672569f4a1de13f9c48b9c39ffc559c71e149
Pavol Hejny <me@pavolhejny.com>
2025-12-05 19:04:39 +0100

    🆚 Update `0.103.0-53` -> `version.ts`
    🔼 This commit was automatically generated by map scripts

--- src/version.ts
+++ src/version.ts
@@ -16,11 +16,11 @@ export const BOOK_LANGUAGE_VERSION: string_semantic_version = '2.0.0';
  * @generated
  * @see https://github.com/webgptorg/promptbook
  */
-export const PROMPTBOOK_ENGINE_VERSION: string_promptbook_version = '0.103.0-52';
+export const PROMPTBOOK_ENGINE_VERSION: string_promptbook_version = '0.103.0-53';
 
 /**
  * Represents the version string of the Promptbook engine.
- * It follows semantic versioning (e.g., `0.103.0-51`).
+ * It follows semantic versioning (e.g., `0.103.0-52`).
  *
  * @generated
  */
