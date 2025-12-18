c091077e0b05a1c8ba725dcc1dc3823965175290
Pavol Hejny <me@pavolhejny.com>
2025-12-18 15:42:57 +0100

    Working Browser

--- apps/playground/src/app/api/test-browser/screenshot/route.ts
+++ apps/playground/src/app/api/test-browser/screenshot/route.ts
@@ -1,4 +1,5 @@
 import { serializeError } from '@promptbook-local/utils';
+import { locateChrome } from 'locate-app';
 import { NextResponse } from 'next/server';
 import { Browser, chromium } from 'playwright';
 import { assertsError } from '../../../../../../../src/errors/assertsError';
@@ -13,6 +14,7 @@ export async function GET() {
         if (!globalForBrowser._browserInstance || !globalForBrowser._browserInstance.isConnected()) {
             console.log('Launching new browser instance...');
             globalForBrowser._browserInstance = await chromium.launch({
+                executablePath: await locateChrome(),
                 headless: false,
             });
         }
