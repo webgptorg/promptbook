0ffbcf66247529a42d9a7fce73a2b72d40bc4df9
Pavol Hejny <me@pavolhejny.com>
2025-12-20 19:33:02 +0100

    Avatars - Allow to pass string url into `generatePlaceholderAgentProfileImageUrl`

--- apps/agents-server/config.ts
+++ apps/agents-server/config.ts
@@ -29,9 +29,7 @@ const config = ConfigChecker.from({
  *
  * Note: When `SERVERS` are used, this URL will be overridden by the server URL.
  */
-export const NEXT_PUBLIC_SITE_URL = config
-    .get('NEXT_PUBLIC_SITE_URL')
-    .url()./* <- TODO: !!!! Is it ok not to be required().*/ value;
+export const NEXT_PUBLIC_SITE_URL = config.get('NEXT_PUBLIC_SITE_URL').url().value;
 
 /**
  * [♐️] Vercel environment: "development" | "preview" | "production"
--- src/book-2.0/utils/generatePlaceholderAgentProfileImageUrl.ts
+++ src/book-2.0/utils/generatePlaceholderAgentProfileImageUrl.ts
@@ -1,5 +1,10 @@
 import { CORE_SERVER } from '../../../servers';
-import type { string_agent_name, string_agent_permanent_id, string_url_image } from '../../types/typeAliases';
+import type {
+    string_agent_name,
+    string_agent_permanent_id,
+    string_url,
+    string_url_image,
+} from '../../types/typeAliases';
 
 /**
  * Generates an image for the agent to use as profile image
@@ -11,9 +16,13 @@ import type { string_agent_name, string_agent_permanent_id, string_url_image } f
  */
 export function generatePlaceholderAgentProfileImageUrl(
     agentIdOrName: string_agent_permanent_id | string_agent_name,
-    agentsServerUrl: URL = new URL(CORE_SERVER.urls[0]!),
+    agentsServerUrl: URL | string_url = CORE_SERVER.urls[0]!,
 ): string_url_image {
-    return `${agentsServerUrl.href}agents/${agentIdOrName}/images/default-avatar.png?nonce=0`;
+    if (typeof agentsServerUrl === 'string') {
+        agentsServerUrl = new URL(agentsServerUrl);
+    }
+
+    return `${agentsServerUrl.href}agents/${agentIdOrName}/images/default-avatar.png`;
 }
 
 /**
