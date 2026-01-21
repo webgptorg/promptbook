b415125b7c4d77f5b5fe720e0e0c7d7b95404ed3
Pavol Hejny <me@pavolhejny.com>
2026-01-21 10:17:29 +0100

    Fix test - Add polyfills for TextEncoder and TextDecoder in jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,9 @@
 import 'cross-fetch/polyfill';
 import * as dotenv from 'dotenv';
+import { TextDecoder, TextEncoder } from 'util';
+
+// Polyfill TextEncoder and TextDecoder for jsdom/whatwg-url
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 dotenv.config({ path: '.env' });
