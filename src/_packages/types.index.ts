264a78350bd7280639b9d4b88aa02d898df44ce7
Pavol Hejny <me@pavolhejny.com>
2026-02-05 08:19:20 +0100

    Autogenerated

--- src/_packages/types.index.ts
+++ src/_packages/types.index.ts
@@ -36,7 +36,6 @@ import type { SourceChipProps } from '../book-components/Chat/SourceChip/SourceC
 import type { ChatToolCall } from '../book-components/Chat/types/ChatMessage';
 import type { ChatMessage } from '../book-components/Chat/types/ChatMessage';
 import type { ChatParticipant } from '../book-components/Chat/types/ChatParticipant';
-import type { ToolCallChipletInfo } from '../book-components/Chat/utils/getToolCallChipletInfo';
 import type { ParsedCitation } from '../book-components/Chat/utils/parseCitationsFromContent';
 import type { MessageButton } from '../book-components/Chat/utils/parseMessageButtons';
 import type { TeamToolResult } from '../book-components/Chat/utils/toolCallParsing';
@@ -441,7 +440,6 @@ export type { SourceChipProps };
 export type { ChatToolCall };
 export type { ChatMessage };
 export type { ChatParticipant };
-export type { ToolCallChipletInfo };
 export type { ParsedCitation };
 export type { MessageButton };
 export type { TeamToolResult };
