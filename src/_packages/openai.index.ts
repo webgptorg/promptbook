6eee98c34cff9d432fc5e913e843e413ed092a49
Pavol Hejny <me@pavolhejny.com>
2026-02-06 09:26:22 +0100

    📦 Generating packages `0.110.0-4`
    🔼 This commit was automatically generated by map scripts

--- src/_packages/openai.index.ts
+++ src/_packages/openai.index.ts
@@ -1,6 +1,7 @@
 // ⚠️ WARNING: This code has been generated so that any manual changes will be overwritten
 // `@promptbook/openai`
 
+import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
 import { createOpenAiAssistantExecutionTools } from '../llm-providers/openai/createOpenAiAssistantExecutionTools';
 import { createOpenAiCompatibleExecutionTools } from '../llm-providers/openai/createOpenAiCompatibleExecutionTools';
 import { createOpenAiExecutionTools } from '../llm-providers/openai/createOpenAiExecutionTools';
@@ -8,40 +9,33 @@ import { OPENAI_MODELS } from '../llm-providers/openai/openai-models';
 import { OpenAiAssistantExecutionTools } from '../llm-providers/openai/OpenAiAssistantExecutionTools';
 import type { OpenAiAssistantExecutionToolsOptions } from '../llm-providers/openai/OpenAiAssistantExecutionToolsOptions';
 import { OpenAiCompatibleExecutionTools } from '../llm-providers/openai/OpenAiCompatibleExecutionTools';
-import type {
-    OpenAiCompatibleExecutionToolsNonProxiedOptions,
-    OpenAiCompatibleExecutionToolsOptions,
-    OpenAiCompatibleExecutionToolsProxiedOptions,
-} from '../llm-providers/openai/OpenAiCompatibleExecutionToolsOptions';
+import type { OpenAiCompatibleExecutionToolsOptions } from '../llm-providers/openai/OpenAiCompatibleExecutionToolsOptions';
+import type { OpenAiCompatibleExecutionToolsNonProxiedOptions } from '../llm-providers/openai/OpenAiCompatibleExecutionToolsOptions';
+import type { OpenAiCompatibleExecutionToolsProxiedOptions } from '../llm-providers/openai/OpenAiCompatibleExecutionToolsOptions';
 import { OpenAiExecutionTools } from '../llm-providers/openai/OpenAiExecutionTools';
 import type { OpenAiExecutionToolsOptions } from '../llm-providers/openai/OpenAiExecutionToolsOptions';
-import {
-    _OpenAiAssistantRegistration,
-    _OpenAiCompatibleRegistration,
-    _OpenAiRegistration,
-} from '../llm-providers/openai/register-constructor';
-import { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION } from '../version';
+import { _OpenAiRegistration } from '../llm-providers/openai/register-constructor';
+import { _OpenAiAssistantRegistration } from '../llm-providers/openai/register-constructor';
+import { _OpenAiCompatibleRegistration } from '../llm-providers/openai/register-constructor';
+
 
 // Note: Exporting version from each package
 export { BOOK_LANGUAGE_VERSION, PROMPTBOOK_ENGINE_VERSION };
 
+
 // Note: Entities of the `@promptbook/openai`
-export {
-    _OpenAiAssistantRegistration,
-    _OpenAiCompatibleRegistration,
-    _OpenAiRegistration,
-    createOpenAiAssistantExecutionTools,
-    createOpenAiCompatibleExecutionTools,
-    createOpenAiExecutionTools,
-    OPENAI_MODELS,
-    OpenAiAssistantExecutionTools,
-    OpenAiCompatibleExecutionTools,
-    OpenAiExecutionTools,
-};
-export type {
-    OpenAiAssistantExecutionToolsOptions,
-    OpenAiCompatibleExecutionToolsNonProxiedOptions,
-    OpenAiCompatibleExecutionToolsOptions,
-    OpenAiCompatibleExecutionToolsProxiedOptions,
-    OpenAiExecutionToolsOptions,
-};
+export { createOpenAiAssistantExecutionTools };
+export { createOpenAiCompatibleExecutionTools };
+export { createOpenAiExecutionTools };
+export { OPENAI_MODELS };
+export { OpenAiAssistantExecutionTools };
+export type { OpenAiAssistantExecutionToolsOptions };
+export { OpenAiCompatibleExecutionTools };
+export type { OpenAiCompatibleExecutionToolsOptions };
+export type { OpenAiCompatibleExecutionToolsNonProxiedOptions };
+export type { OpenAiCompatibleExecutionToolsProxiedOptions };
+export { OpenAiExecutionTools };
+export type { OpenAiExecutionToolsOptions };
+export { _OpenAiRegistration };
+export { _OpenAiAssistantRegistration };
+export { _OpenAiCompatibleRegistration };
