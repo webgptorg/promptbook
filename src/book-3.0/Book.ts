7ee6ba63bf1cb78fc0699de99309e985b7e47b36
Pavol Hejny <me@pavolhejny.com>
2026-05-13 18:50:12 +0200

    Fix lint

--- src/book-3.0/Book.ts
+++ src/book-3.0/Book.ts
@@ -1,6 +1,7 @@
 import type { string_book } from '../book-2.0/agent-source/string_book';
 import type { ChatMessage } from '../book-components/Chat/types/ChatMessage';
 import { book } from '../pipeline/book-notation';
+import { TODO_USE } from '../utils/organization/TODO_USE';
 
 /**
  * Parser for the agent source for Promptbook agents in book language
@@ -10,6 +11,8 @@ import { book } from '../pipeline/book-notation';
 export class Book {
     static parse(source: string_book): Book {
         // TODO: Implement
+
+        TODO_USE(/* TODO: Implement parsing logic */ source);
 
         return new Book('TODO', []);
     }
